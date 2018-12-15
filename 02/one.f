5d490e739bcd151c77948ac604f1da33d0167e0f
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2018-12-14 21:28:49 -0800

    done with part one

--- 02/one.f
+++ 02/one.f
@@ -4,17 +4,17 @@
       parameter ( label_len = 26, max_lines = 1024 )
 
 !      character (len=label_len), dimension(max_lines) ::  sarray
-      character (len=label_len) :: s, done
+      character (len=label_len) :: s
       integer n, n2, n3
       n2 = 0
       n3 = 0
-      done= ''
 
       n = 1
       open(10,file='scan.txt',status='old')
       do while ( .true. )
        read(10,*,end=100) s
-       call counter(trim(s),trim(done),n2,n3)
+!       write(6,*) s
+       call counter(trim(s),n2,n3)
        n = n + 1
       end do
 100   close(10)
@@ -32,25 +32,34 @@
 !
 !
 !
-      subroutine counter(a, done, n2, n3)
+      subroutine counter(a, n2, n3)
       implicit none
-      character a*(*), done*(*)
+      character a*(*)
       integer n2, n3
       integer i, j, occ
-      character s*(1)
+      character s*(1), done*(26)
+      logical twos, threes
 !      character alpha*26
 !      data alpha / 'abcdefghijklmnopqrstuvwxyz' /
+      done= ''
+      twos = .true.
+      threes = .true.
       do i = 1, len(a)
        occ = 0
        s = a(i:i)
-        write(6,*) ' done=',done, ' s=',s, scan(done,s)
+!        write(6,*) ' done=',done, ' s=',s, scan(done,s)
        if ( scan(done,s) .gt. 0 ) goto 90
        do j = i, len(a)
-        write(6,*) ' a=',a, ' s=',s, ' a(j)=', a(j:j), ' occ=',occ
+!        write(6,*) ' a=',a, ' s=',s, ' a(j)=', a(j:j), ' occ=',occ
         if ( s .eq. a(j:j) ) occ = occ + 1
        end do
-       if ( occ .eq. 2 ) n2 = n2 + 1
-       if ( occ .eq. 3 ) n3 = n3 + 1
+       if ( occ .eq. 2 .and. twos ) then
+        n2 = n2 + 1
+        twos = .false.
+       else if ( occ .eq. 3 .and. threes ) then
+        n3 = n3 + 1
+        threes = .false.
+       endif
        done = trim(done)//s
 !       write(6,*) done
  90    continue
