3e7ab7929b78b88b6e69c159a1cd9af554a62476
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2018-12-16 09:58:07 -0800

    first crack at operations

--- 16/one.c
+++ 16/one.c
@@ -2,32 +2,65 @@
 #include<stdlib.h>
 
 typedef struct {
- int R0,R1,R2,R3;
+ int R[4];
 } reg;
 
-void addr (int* A, int* B, int* C) {
-	*C = *A + *B;
-	return;
-}
-
-void addi (int* A, int B, int* C) {
-	*C = *A + B;
-	return;
-}
+void addr (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] + (*R).R[(*R).R[1]]; return; }
+void addi (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] + (*R).R[1]; return; }
+void mulr (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] * (*R).R[(*R).R[1]]; return; }
+void muli (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] * (*R).R[1]; return; }
+void banr (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] & (*R).R[(*R).R[1]]; return; }
+void bani (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] & (*R).R[1]; return; }
+void borr (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] | (*R).R[(*R).R[1]]; return; }
+void bori (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]] | (*R).R[1]; return; }
+void setr (reg* R) { (*R).R[(*R).R[2]] = (*R).R[(*R).R[0]]; return; }
+void seti (reg* R) { (*R).R[(*R).R[2]] = (*R).R[0]; return; }
+void gtir (reg* R) {
+ if ( (*R).R[0] > (*R).R[(*R).R[1]] ) { (*R).R[(*R).R[2]] = 1; return; }
+ else { (*R).R[(*R).R[2]] = 0; return; } }
+void gtri (reg* R) {
+ if ( (*R).R[(*R).R[0]] > (*R).R[1] ) { (*R).R[(*R).R[2]] = 1; return; }
+ else { (*R).R[(*R).R[2]] = 0; return; } }
+void gtrr (reg* R) {
+ if ( (*R).R[(*R).R[0]] > (*R).R[(*R).R[1]] ) { (*R).R[(*R).R[2]] = 1; return; }
+ else { (*R).R[(*R).R[2]] = 0; return; } }
+void eqir (reg* R) {
+ if ( (*R).R[0] == (*R).R[(*R).R[1]] ) { (*R).R[(*R).R[2]] = 1; return; }
+ else { (*R).R[(*R).R[2]] = 0; return; } }
+void eqri (reg* R) {
+ if ( (*R).R[(*R).R[0]] == (*R).R[1] ) { (*R).R[(*R).R[2]] = 1; return; }
+ else { (*R).R[(*R).R[2]] = 0; return; } }
+void eqrr (reg* R) {
+ if ( (*R).R[(*R).R[0]] == (*R).R[(*R).R[1]] ) { (*R).R[(*R).R[2]] = 1; return; }
+ else { (*R).R[(*R).R[2]] = 0; return; } }
 
 int main (void) {
  reg R;
 
- R.R0 = 1;
- R.R1 = 2;
- R.R2 = 3;
- R.R3 = 4;
+// R.R0 = 0x1100;
+// R.R1 = 0x0101;
+// R.R2 = 0x0011;
+// R.R3 = 0x0100;
+ R.R[0] =  0; R.R[1] =  7; R.R[2] =  3; R.R[3] =  1;
 
- printf("\n before addr: [ %2d, %2d, %2d, %2d ]\n\n", R.R0, R.R1, R.R2, R.R3);
+ printf("\n before addi: [ %2d, %2d, %2d, %2d ]\n", R.R[0], R.R[1], R.R[2], R.R[3]);
+ addi ( &R );
+ printf("\n  after addi: [ %2d, %2d, %2d, %2d ]\n\n", R.R[0], R.R[1], R.R[2], R.R[3]);
 
- addr ( &(R.R0), &(R.R1), &(R.R2) );
+ R.R[0] =  3; R.R[1] =  2; R.R[2] =  1; R.R[3] =  1;
+ printf("\n before mulr: [ %2d, %2d, %2d, %2d ]\n", R.R[0], R.R[1], R.R[2], R.R[3]);
+ mulr ( &R );
+ printf("\n  after mulr: [ %2d, %2d, %2d, %2d ]\n\n", R.R[0], R.R[1], R.R[2], R.R[3]);
 
- printf("\n  after addr: [ %2d, %2d, %2d, %2d ]\n\n", R.R0, R.R1, R.R2, R.R3);
+ R.R[0] =  3; R.R[1] =  2; R.R[2] =  1; R.R[3] =  1;
+ printf("\n before addi: [ %2d, %2d, %2d, %2d ]\n", R.R[0], R.R[1], R.R[2], R.R[3]);
+ addi ( &R );
+ printf("\n  after addi: [ %2d, %2d, %2d, %2d ]\n\n", R.R[0], R.R[1], R.R[2], R.R[3]);
+
+ R.R[0] =  3; R.R[1] =  2; R.R[2] =  1; R.R[3] =  1;
+ printf("\n before seti: [ %2d, %2d, %2d, %2d ]\n", R.R[0], R.R[1], R.R[2], R.R[3]);
+ seti ( &R );
+ printf("\n  after seti: [ %2d, %2d, %2d, %2d ]\n\n", R.R[0], R.R[1], R.R[2], R.R[3]);
 
  return 0;
 }
