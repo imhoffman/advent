d34556c052a9327f86cfb38239df116026f9bda0
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2018-12-20 12:17:45 -0800

    new two

--- 12/two.f
+++ 12/two.f
@@ -21,74 +21,23 @@
       end do
       close(10)
 
-! use scratch files rather than strings
-!      open(21,status='scratch')    ! initial state
-      open(21,file='junk.txt')    ! initial state
-      open(31,status='scratch')    ! final state
-      open(41,status='scratch')    ! empty state
-
-      do i = 1, ngen
-       call fseek(21,i,0,0)
-       write(21,'(A1)') '.'
-!       write(21,'(A1)',advance='no') '.'
-      end do
-       call fseek(21,ngen,0,0)
-      write(21,fmt2) trim(state0)
-!      write(21,fmt2,advance='no') trim(state0)
-      do i = 1, ngen+2
-       call fseek(21,ngen+npot0+i,0,0)
-       write(21,'(A1)') '.'
-!       write(21,'(A1)',advance='no') '.'
-      end do
-      rewind(21)
-
       do i = 0, npots-1
-       call fseek(41,i,0,0)
-       write(41,'(A1)') '.'
       end do
-      write(6,*) ' is rewind the problem?'
-      rewind(41)
-      write(6,*) ' is rewind the problem?'
 
       do i = 2, ngen
        do jj = 0, npots-1      ! 0 b/c offsets
-        call fseek(31,jj,0,0)
-        call fseek(41,jj,0,0)
-        read(41, '(A1)', advance='no') pot
-        write(31,'(A1)', advance='no') pot
        end do
-       rewind(41)
        do j = 3, npots-m
-        call fseek(21,j-3,0,0)
-        read(21,'(A5)',advance='no') nigh
         do k = 1, nrule
-         if ( nigh .eq. grow(k,1) ) then
-          call fseek(31,j,0,0)
-          write(31,'(A1)',advance='no') grow(k,2)
-         end if
         end do
        end do
        do jj = 0, npots-1
-        call fseek(21,jj,0,0)
-        call fseek(31,jj,0,0)
-        read(31, '(A1)', advance='no') pot
-        write(21,'(A1)', advance='no') pot
        end do
-       rewind(21)
-       rewind(31)
       end do
-      close(21)
-      close(41)
 
       total = 0
       do i = 0, npots-1
-       call fseek(31,i,0,0)
-       read(31,'(A1)',advance='no') pot
-       if ( pot .eq. '#' ) then
-        total = total + ival(i)
-       end if
       end do
-      close(31)
       write(6,*) 'sum of plant indices:', total
 
       stop
