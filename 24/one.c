7773b00b6451486cf74dc44cb6a3620fa60f1fbd
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2018-12-24 09:14:08 -0800

    armies array

--- 24/one.c
+++ 24/one.c
@@ -15,13 +15,14 @@ typedef struct {
  int army, units, adam, atype, init, hp, weak, imm, order, attacking;
 } group;
 
-void data_entry ( group* immune, group* infect );
+void data_entry ( group* armies );
 
-void target_selection ( group* mune, group* fect ) {
- int n=sizeof(mune)/sizeof(mune[0]), m=sizeof(fect)/sizeof(fect[0]);
+void target_selection ( group* a ) {
+ int n=sizeof(a)/sizeof(a[0]);
  int i, j, k, higher=INT_MAX, current=0, turn=0;
 
- mune[2].attacking = 5;
+ a[2].attacking = 5;
+ a[2].order = 12;
  return;
 }
 
@@ -31,13 +32,15 @@ int main (void) {
 
  group armies[20];
  data_entry( armies );
-// n = 3;
-// printf("\n immune group %1d has weaknesses 0x%04x and immunities 0x%04x\n",n,immune[n].weak,immune[n].imm);
-// n = 6;
-// printf("\n infect group %1d has weaknesses 0x%04x and immunities 0x%04x\n",n,infect[n].weak,infect[n].imm);
+
+ n = 12;
+ printf(" group %2d is in army %d and has weaknesses 0x%04x and immunities 0x%04x\n",n,armies[n].army,armies[n].weak,armies[n].imm);
+ n = 07;
+ printf(" group %2d is in army %d and has weaknesses 0x%04x and immunities 0x%04x\n",n,armies[n].army,armies[n].weak,armies[n].imm);
 
  target_selection( armies );
-// printf("\n\n immune group %1d is attacking infect group %1d\n\n",2,immune[2].attacking);
+ n = 02;
+ printf(" group %2d is in army %d and is attacking group %2d with rank %2d\n",n,armies[n].army,armies[n].attacking,armies[n].order);
 
  return 0;
 }
@@ -46,124 +49,124 @@ void data_entry ( group* armies ) {
  int n;
 
  n = 0; armies[n].army=army0;
- immune[n].units=479; immune[n].hp=3393; immune[n].init=8; immune[n].adam=66;
- immune[n].atype=0; immune[n].atype |= (1<<cold);
- immune[n].weak=0; immune[n].weak |= (1<<radiation);
- immune[n].imm=0;
+ armies[n].units=479; armies[n].hp=3393; armies[n].init=8; armies[n].adam=66;
+ armies[n].atype=0; armies[n].atype |= (1<<cold);
+ armies[n].weak=0; armies[n].weak |= (1<<radiation);
+ armies[n].imm=0;
 
- n = 1;
- immune[n].units=2202; immune[n].hp=4950; immune[n].init=2; immune[n].adam=18;
- immune[n].atype=0; immune[n].atype |= (1<<cold);
- immune[n].weak=0; immune[n].weak |= (1<<fire);
- immune[n].imm=0; immune[n].imm |= (1<<slashing);
+ n = 1; armies[n].army=army0;
+ armies[n].units=2202; armies[n].hp=4950; armies[n].init=2; armies[n].adam=18;
+ armies[n].atype=0; armies[n].atype |= (1<<cold);
+ armies[n].weak=0; armies[n].weak |= (1<<fire);
+ armies[n].imm=0; armies[n].imm |= (1<<slashing);
 
- n = 2;
- immune[n].units=8132; immune[n].hp=9680; immune[n].init=7; immune[n].adam=9;
- immune[n].atype=0; immune[n].atype |= (1<<radiation);
- immune[n].weak=0; immune[n].weak |= (1<<bludgeoning); immune[n].weak |= (1<<fire);
- immune[n].imm=0; immune[n].imm |= (1<<slashing);
+ n = 2; armies[n].army=army0;
+ armies[n].units=8132; armies[n].hp=9680; armies[n].init=7; armies[n].adam=9;
+ armies[n].atype=0; armies[n].atype |= (1<<radiation);
+ armies[n].weak=0; armies[n].weak |= (1<<bludgeoning); armies[n].weak |= (1<<fire);
+ armies[n].imm=0; armies[n].imm |= (1<<slashing);
 
- n = 3;
- immune[n].units=389; immune[n].hp=13983; immune[n].init=13; immune[n].adam=256;
- immune[n].atype=0; immune[n].atype |= (1<<cold);
- immune[n].weak=0;
- immune[n].imm=0; immune[n].imm |= (1<<bludgeoning);
+ n = 3; armies[n].army=army0;
+ armies[n].units=389; armies[n].hp=13983; armies[n].init=13; armies[n].adam=256;
+ armies[n].atype=0; armies[n].atype |= (1<<cold);
+ armies[n].weak=0;
+ armies[n].imm=0; armies[n].imm |= (1<<bludgeoning);
 
- n = 4;
- immune[n].units=1827; immune[n].hp=5107; immune[n].init=18; immune[n].adam=24;
- immune[n].atype=0; immune[n].atype |= (1<<slashing);
- immune[n].weak=0;
- immune[n].imm=0;
+ n = 4; armies[n].army=army0;
+ armies[n].units=1827; armies[n].hp=5107; armies[n].init=18; armies[n].adam=24;
+ armies[n].atype=0; armies[n].atype |= (1<<slashing);
+ armies[n].weak=0;
+ armies[n].imm=0;
 
- n = 5;
- immune[n].units=7109; immune[n].hp=2261; immune[n].init=16; immune[n].adam=3;
- immune[n].atype=0; immune[n].atype |= (1<<fire);
- immune[n].weak=0;
- immune[n].imm=0; immune[n].imm |= (1<<radiation); immune[n].imm |= (1<<slashing); immune[n].imm |= (1<<cold);
+ n = 5; armies[n].army=army0;
+ armies[n].units=7109; armies[n].hp=2261; armies[n].init=16; armies[n].adam=3;
+ armies[n].atype=0; armies[n].atype |= (1<<fire);
+ armies[n].weak=0;
+ armies[n].imm=0; armies[n].imm |= (1<<radiation); armies[n].imm |= (1<<slashing); armies[n].imm |= (1<<cold);
 
- n = 6;
- immune[n].units=4736; immune[n].hp=8421; immune[n].init=3; immune[n].adam=17;
- immune[n].atype=0; immune[n].atype |= (1<<slashing);
- immune[n].weak=0; immune[n].weak |= (1<<cold);
- immune[n].imm=0;
+ n = 6; armies[n].army=army0;
+ armies[n].units=4736; armies[n].hp=8421; armies[n].init=3; armies[n].adam=17;
+ armies[n].atype=0; armies[n].atype |= (1<<slashing);
+ armies[n].weak=0; armies[n].weak |= (1<<cold);
+ armies[n].imm=0;
 
- n = 7;
- immune[n].units=491; immune[n].hp=3518; immune[n].init=1; immune[n].adam=65;
- immune[n].atype=0; immune[n].atype |= (1<<radiation);
- immune[n].weak=0; immune[n].weak |= (1<<cold);
- immune[n].imm=0; immune[n].imm |= (1<<bludgeoning); immune[n].imm |= (1<<fire);
+ n = 7; armies[n].army=army0;
+ armies[n].units=491; armies[n].hp=3518; armies[n].init=1; armies[n].adam=65;
+ armies[n].atype=0; armies[n].atype |= (1<<radiation);
+ armies[n].weak=0; armies[n].weak |= (1<<cold);
+ armies[n].imm=0; armies[n].imm |= (1<<bludgeoning); armies[n].imm |= (1<<fire);
 
- n = 8;
- immune[n].units=2309; immune[n].hp=7353; immune[n].init=20; immune[n].adam=31;
- immune[n].atype=0; immune[n].atype |= (1<<bludgeoning);
- immune[n].weak=0;
- immune[n].imm=0; immune[n].imm |= (1<<radiation);
+ n = 8; armies[n].army=army0;
+ armies[n].units=2309; armies[n].hp=7353; armies[n].init=20; armies[n].adam=31;
+ armies[n].atype=0; armies[n].atype |= (1<<bludgeoning);
+ armies[n].weak=0;
+ armies[n].imm=0; armies[n].imm |= (1<<radiation);
 
- n = 9;
- immune[n].units=411; immune[n].hp=6375; immune[n].init=14; immune[n].adam=151;
- immune[n].atype=0; immune[n].atype |= (1<<bludgeoning);
- immune[n].weak=0; immune[n].weak |= (1<<cold); immune[n].weak |= (1<<fire);
- immune[n].imm=0; immune[n].imm |= (1<<slashing);
+ n = 9; armies[n].army=army0;
+ armies[n].units=411; armies[n].hp=6375; armies[n].init=14; armies[n].adam=151;
+ armies[n].atype=0; armies[n].atype |= (1<<bludgeoning);
+ armies[n].weak=0; armies[n].weak |= (1<<cold); armies[n].weak |= (1<<fire);
+ armies[n].imm=0; armies[n].imm |= (1<<slashing);
 
- n = 0;
- infect[n].units=148; infect[n].hp=31914; infect[n].init=4; infect[n].adam=416;
- infect[n].atype=0; infect[n].atype |= (1<<cold);
- infect[n].weak=0; infect[n].weak |= (1<<bludgeoning);
- infect[n].imm=0; infect[n].imm |= (1<<radiation); infect[n].imm |= (1<<cold); infect[n].imm |= (1<<fire);
+ n = 10; armies[n].army=army1;
+ armies[n].units=148; armies[n].hp=31914; armies[n].init=4; armies[n].adam=416;
+ armies[n].atype=0; armies[n].atype |= (1<<cold);
+ armies[n].weak=0; armies[n].weak |= (1<<bludgeoning);
+ armies[n].imm=0; armies[n].imm |= (1<<radiation); armies[n].imm |= (1<<cold); armies[n].imm |= (1<<fire);
 
- n = 1;
- infect[n].units=864; infect[n].hp=38189; infect[n].init=6; infect[n].adam=72;
- infect[n].atype=0; infect[n].atype |= (1<<slashing);
- infect[n].weak=0;
- infect[n].imm=0;
+ n = 11; armies[n].army=army1;
+ armies[n].units=864; armies[n].hp=38189; armies[n].init=6; armies[n].adam=72;
+ armies[n].atype=0; armies[n].atype |= (1<<slashing);
+ armies[n].weak=0;
+ armies[n].imm=0;
 
- n = 2;
- infect[n].units=2981; infect[n].hp=7774; infect[n].init=15; infect[n].adam=4;
- infect[n].atype=0; infect[n].atype |= (1<<fire);
- infect[n].weak=0;
- infect[n].imm=0; infect[n].imm |= (1<<cold); infect[n].imm |= (1<<bludgeoning);
+ n = 12; armies[n].army=army1;
+ armies[n].units=2981; armies[n].hp=7774; armies[n].init=15; armies[n].adam=4;
+ armies[n].atype=0; armies[n].atype |= (1<<fire);
+ armies[n].weak=0;
+ armies[n].imm=0; armies[n].imm |= (1<<cold); armies[n].imm |= (1<<bludgeoning);
 
- n = 3;
- infect[n].units=5259; infect[n].hp=22892; infect[n].init=5; infect[n].adam=8;
- infect[n].atype=0; infect[n].atype |= (1<<fire);
- infect[n].weak=0;
- infect[n].imm=0;
+ n = 13; armies[n].army=army1;
+ armies[n].units=5259; armies[n].hp=22892; armies[n].init=5; armies[n].adam=8;
+ armies[n].atype=0; armies[n].atype |= (1<<fire);
+ armies[n].weak=0;
+ armies[n].imm=0;
 
- n = 4;
- infect[n].units=318; infect[n].hp=16979; infect[n].init=9; infect[n].adam=106;
- infect[n].atype=0; infect[n].atype |= (1<<bludgeoning);
- infect[n].weak=0; infect[n].weak |= (1<<fire);
- infect[n].imm=0;
+ n = 14; armies[n].army=army1;
+ armies[n].units=318; armies[n].hp=16979; armies[n].init=9; armies[n].adam=106;
+ armies[n].atype=0; armies[n].atype |= (1<<bludgeoning);
+ armies[n].weak=0; armies[n].weak |= (1<<fire);
+ armies[n].imm=0;
 
- n = 5;
- infect[n].units=5017; infect[n].hp=32175; infect[n].init=17; infect[n].adam=11;
- infect[n].atype=0; infect[n].atype |= (1<<bludgeoning);
- infect[n].weak=0; infect[n].weak |= (1<<slashing);
- infect[n].imm=0; infect[n].imm |= (1<<radiation);
+ n = 15; armies[n].army=army1;
+ armies[n].units=5017; armies[n].hp=32175; armies[n].init=17; armies[n].adam=11;
+ armies[n].atype=0; armies[n].atype |= (1<<bludgeoning);
+ armies[n].weak=0; armies[n].weak |= (1<<slashing);
+ armies[n].imm=0; armies[n].imm |= (1<<radiation);
 
- n = 6;
- infect[n].units=4308; infect[n].hp=14994; infect[n].init=10; infect[n].adam=5;
- infect[n].atype=0; infect[n].atype |= (1<<fire);
- infect[n].weak=0; infect[n].weak |= (1<<slashing);
- infect[n].imm=0; infect[n].imm |= (1<<fire); infect[n].imm |= (1<<cold);
+ n = 16; armies[n].army=army1;
+ armies[n].units=4308; armies[n].hp=14994; armies[n].init=10; armies[n].adam=5;
+ armies[n].atype=0; armies[n].atype |= (1<<fire);
+ armies[n].weak=0; armies[n].weak |= (1<<slashing);
+ armies[n].imm=0; armies[n].imm |= (1<<fire); armies[n].imm |= (1<<cold);
 
- n = 7;
- infect[n].units=208; infect[n].hp=14322; infect[n].init=19; infect[n].adam=133;
- infect[n].atype=0; infect[n].atype |= (1<<cold);
- infect[n].weak=0; infect[n].weak |= (1<<radiation);
- infect[n].imm=0;
+ n = 17; armies[n].army=army1;
+ armies[n].units=208; armies[n].hp=14322; armies[n].init=19; armies[n].adam=133;
+ armies[n].atype=0; armies[n].atype |= (1<<cold);
+ armies[n].weak=0; armies[n].weak |= (1<<radiation);
+ armies[n].imm=0;
 
- n = 8;
- infect[n].units=3999; infect[n].hp=48994; infect[n].init=11; infect[n].adam=20;
- infect[n].atype=0; infect[n].atype |= (1<<cold);
- infect[n].weak=0; infect[n].weak |= (1<<slashing); infect[n].weak |= (1<<cold);
- infect[n].imm=0;
+ n = 18; armies[n].army=army1;
+ armies[n].units=3999; armies[n].hp=48994; armies[n].init=11; armies[n].adam=20;
+ armies[n].atype=0; armies[n].atype |= (1<<cold);
+ armies[n].weak=0; armies[n].weak |= (1<<slashing); armies[n].weak |= (1<<cold);
+ armies[n].imm=0;
 
- n = 9;
- infect[n].units=1922; infect[n].hp=34406; infect[n].init=12; infect[n].adam=35;
- infect[n].atype=0; infect[n].atype |= (1<<slashing);
- infect[n].weak=0; infect[n].weak |= (1<<slashing);
- infect[n].imm=0;
+ n = 19; armies[n].army=army1;
+ armies[n].units=1922; armies[n].hp=34406; armies[n].init=12; armies[n].adam=35;
+ armies[n].atype=0; armies[n].atype |= (1<<slashing);
+ armies[n].weak=0; armies[n].weak |= (1<<slashing);
+ armies[n].imm=0;
 
  return;
 }
