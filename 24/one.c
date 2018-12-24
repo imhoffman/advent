63d42cebaf339e69fbe30b0394035c7c335cc52c
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2018-12-23 22:47:09 -0800

    completed input

--- 24/one.c
+++ 24/one.c
@@ -91,8 +91,64 @@ int main (void) {
  infect[n].weak=0; infect[n].weak |= (1<<bludgeoning);
  infect[n].imm=0; infect[n].imm |= (1<<radiation); infect[n].imm |= (1<<cold); infect[n].imm |= (1<<fire);
 
+ n = 1;
+ infect[n].units=864; infect[n].hp=38189; infect[n].init=6; infect[n].adam=72;
+ infect[n].atype=0; infect[n].atype |= (1<<slashing);
+ infect[n].weak=0;
+ infect[n].imm=0;
 
+ n = 2;
+ infect[n].units=2981; infect[n].hp=7774; infect[n].init=15; infect[n].adam=4;
+ infect[n].atype=0; infect[n].atype |= (1<<fire);
+ infect[n].weak=0;
+ infect[n].imm=0; infect[n].imm |= (1<<cold); infect[n].imm |= (1<<bludgeoning);
 
+ n = 3;
+ infect[n].units=5259; infect[n].hp=22892; infect[n].init=5; infect[n].adam=8;
+ infect[n].atype=0; infect[n].atype |= (1<<fire);
+ infect[n].weak=0;
+ infect[n].imm=0;
+
+ n = 4;
+ infect[n].units=318; infect[n].hp=16979; infect[n].init=9; infect[n].adam=106;
+ infect[n].atype=0; infect[n].atype |= (1<<bludgeoning);
+ infect[n].weak=0; infect[n].weak |= (1<<fire);
+ infect[n].imm=0;
+
+ n = 5;
+ infect[n].units=5017; infect[n].hp=32175; infect[n].init=17; infect[n].adam=11;
+ infect[n].atype=0; infect[n].atype |= (1<<bludgeoning);
+ infect[n].weak=0; infect[n].weak |= (1<<slashing);
+ infect[n].imm=0; infect[n].imm |= (1<<radiation);
+
+ n = 6;
+ infect[n].units=4308; infect[n].hp=14994; infect[n].init=10; infect[n].adam=5;
+ infect[n].atype=0; infect[n].atype |= (1<<fire);
+ infect[n].weak=0; infect[n].weak |= (1<<slashing);
+ infect[n].imm=0; infect[n].imm |= (1<<fire); infect[n].imm |= (1<<cold);
+
+ n = 7;
+ infect[n].units=208; infect[n].hp=14322; infect[n].init=19; infect[n].adam=133;
+ infect[n].atype=0; infect[n].atype |= (1<<cold);
+ infect[n].weak=0; infect[n].weak |= (1<<radiation);
+ infect[n].imm=0;
+
+ n = 8;
+ infect[n].units=3999; infect[n].hp=48994; infect[n].init=11; infect[n].adam=20;
+ infect[n].atype=0; infect[n].atype |= (1<<cold);
+ infect[n].weak=0; infect[n].weak |= (1<<slashing); infect[n].weak |= (1<<cold);
+ infect[n].imm=0;
+
+ n = 9;
+ infect[n].units=1922; infect[n].hp=34406; infect[n].init=12; infect[n].adam=35;
+ infect[n].atype=0; infect[n].atype |= (1<<slashing);
+ infect[n].weak=0; infect[n].weak |= (1<<slashing);
+ infect[n].imm=0;
+
+ n = 4;
+ printf("\n\n infect group %1d has a weakness of 0x%04x\n\n",n,infect[n].weak);
+ n = 5;
+ printf("\n\n infect group %1d has %d hp for %d units\n\n",n,infect[n].hp,infect[n].units);
 
  return 0;
 }
