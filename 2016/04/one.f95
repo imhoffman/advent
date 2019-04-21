9f8b344318d91051009c5e4548f36ad314fc8a15
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-04-21 10:10:41 -0700

    ruleset subroutine done

--- 2016/04/one.f95
+++ 2016/04/one.f95
@@ -1,3 +1,7 @@
+!! *** Advent of Code 2016 Day 04
+!!
+!! typedefs and parameters
+!!
  module types
   use iso_fortran_env
   implicit none
@@ -14,11 +18,15 @@
   end type record
  end module types
 
+!!
+!! subroutines and functions
+!!
  module subs
   use types
   implicit none
   contains
 
+  !! read external file and count lines
   subroutine reader ( fp, n, lines )
    implicit none
    integer, intent(in) :: fp
@@ -37,6 +45,7 @@
    return
   end subroutine reader
 
+  !! letter-occurence counter
   recursive function counter ( n, ch, str ) result ( k )
    integer, intent(in)             :: n       ! the running count when called
    character ( len=1 ), intent(in) :: ch
@@ -55,79 +64,83 @@
    return
   end function counter
 
-  subroutine totaller ( g )
-   type(record), dimension(:), intent(in) :: g
-   integer :: n
+  !! puzzle ruleset
+  subroutine totaller ( g, x )
+   type(record), dimension(:) :: g
+   integer (kind=iw) :: x
+   character (len=max_checksum) :: checksum
+   integer :: i, j, k, m1, m2, m3, mm, nthis, nnext, nlast
 
-   n = size( g )
+   do i = 1, size( g )
+     m1 = scan( g(i)%listing, "[" )
+     m2 = scan( g(i)%listing, "]" )
+
+     ! poor man's is_number...
+     m3 = 1024
+     do j = 1, len(numer)
+       mm = scan( g(i)%listing(:m1-1), numer(j:j) )
+       if ( mm .lt. m3  .and.  mm .gt. 0 ) m3 = mm
+     end do
+     ! store value
+     if ( m3 .gt. 0 ) read( g(i)%listing(m3:m1-1), * ) g(i)%val
+
+     g(i)%checksum = g(i)%listing(m1+1:m2-1)
+     do k = 1, m2-m1-2
+       nthis = counter(0,       g(i)%checksum(k:k)       ,g(i)%listing(:m1-1))
+       nnext = counter(0,     g(i)%checksum(k+1:k+1)     ,g(i)%listing(:m1-1))
+       nlast = counter(0, g(i)%checksum(m2-m1-1:m2-m1-1) ,g(i)%listing(:m1-1))
+       ! rule set
+       if ( &
+           &         nlast .gt. 0 &
+           & .and. ( nthis .gt. nnext &
+           &        .or. ( &
+           &              nthis .eq. nnext &
+           &  .and. ( scan( alpha, g(i)%checksum(k:k) ) &
+           &             .lt. &
+           &          scan( alpha, g(i)%checksum(k+1:k+1) ) ) &
+           & ) ) ) then
+         continue
+       else
+         goto 420
+       end if
+     end do
+     x = x + g(i)%val
+     420 continue
+    end do
 
    return
   end subroutine totaller
-
  end module subs
 
+!!
+!! main program
+!!
  program main
   use types
   use subs
   implicit none
   character (len=strlen), dimension(:), allocatable :: temp
   type(record), dimension(:), allocatable :: registry
-  character (len=max_checksum) :: checksum
-  integer :: fileunit=10, Nrooms, nthis, nnext, nlast
+  integer :: fileunit=10, Nrooms
   integer (kind=iw) :: total=0
-  integer :: i, j, k, m1, m2, m3, mm
+  integer :: i
 
+  ! read external file into temporary array
   allocate( temp(max_lines) )
   open(fileunit,file="input.txt")
   call reader ( fileunit, Nrooms, temp )
   close(fileunit)
-  allocate ( registry(Nrooms) )
 
-  !call totaller( registry )
+  ! populate registry and free reading buffer
+  allocate ( registry(Nrooms) )
   do i = 1, Nrooms
-   !registry(i)%listing = ''
    registry(i)%listing = temp(i)
-   !registry(i)%listing = trim( registry(i)%listing )
   end do
   deallocate( temp )
 
-  do i = 1, Nrooms
-   m1 = scan( registry(i)%listing, "[" )
-   m2 = scan( registry(i)%listing, "]" )
-
-   ! poor man's is_number...
-   m3 = 1024
-   do j = 1, len(numer)
-    mm = scan( registry(i)%listing(:m1-1), numer(j:j) )
-    if ( mm .lt. m3  .and.  mm .gt. 0 ) m3 = mm
-   end do
-   ! store value
-   if ( m3 .gt. 0 ) read( registry(i)%listing(m3:m1-1), * ) registry(i)%val
-
-   registry(i)%checksum = registry(i)%listing(m1+1:m2-1)
-   do k = 1, m2-m1-2
-     nthis = counter(0,       registry(i)%checksum(k:k)       ,registry(i)%listing(:m1-1))
-     nnext = counter(0,     registry(i)%checksum(k+1:k+1)     ,registry(i)%listing(:m1-1))
-     nlast = counter(0, registry(i)%checksum(m2-m1-1:m2-m1-1) ,registry(i)%listing(:m1-1))
-     ! rule set
-     if ( &
-         &         nlast .gt. 0 &
-         & .and. ( nthis .gt. nnext &
-         &        .or. ( &
-         &              nthis .eq. nnext &
-         &  .and. ( scan( alpha, registry(i)%checksum(k:k) ) &
-         &             .lt. &
-         &          scan( alpha, registry(i)%checksum(k+1:k+1) ) ) &
-         & ) ) ) then
-       continue
-     else
-       goto 420
-     end if
-   end do
-   total = total + registry(i)%val
-   420 continue
-  end do
-
+  ! compute sum as per puzzle rules
+  total = 0
+  call totaller( registry, total )
   write(6,'(A,I0)') ' total = ', total
 
   deallocate ( registry )
