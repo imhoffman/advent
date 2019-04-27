20de7527613f90071bd7d683f19968e77cd69e4c
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-04-26 20:21:51 -0700

    more python

--- 2016/04/two.py
+++ 2016/04/two.py
@@ -1,15 +1,28 @@
-# https://stackoverflow.com/questions/2682745/how-do-i-create-a-constant-in-python/2682752
-#   "Just don't change it"
 import sys
 def printf(format, *args):
     sys.stdout.write(format % args)
 
+# constants
 class C():
     def pi():
-        return 3.14159
+        return 3.1415926535897932384626433832795028841971
     def alpha():
         return "abcdefghijklmnopqrstuvwxyz"
 
+def counter ( ch, string ):
+    return sum( [ x is ch for x in string ] )
+
+def sumcheck ( x, y ):
+    n = len(y)
+    for i in range(n-1):
+      if ( x.count(y[-1]) > 0 and
+           ( ( x.count(y[i]) > x.count(y[i+1]) ) or
+             ( ( x.count(y[i]) == x.count(y[i+1]) ) and
+               ( C.alpha().index(y[i]) > C.alpha().index(y[i+1]) )
+             )
+           ) ): continue
+      else: return False
+    return True
 
 # poor man's struct
 #  The following mutable lists will be the members
@@ -24,16 +37,37 @@ checksum = []
 is_real = []
 
 registry = (
-   listing,
-   sector_id,
-   encrypted,
-   decrypted,
-   checksum,
-   is_real
+   listing,    # 0
+   sector_id,  # 1
+   encrypted,  # 2
+   decrypted,  # 3
+   checksum,   # 4
+   is_real     # 5
    )
 
-n = 15
-print ( f" the {n:2d}th letter of alpha() is {C.alpha()[n-1]!r}\n" )
-printf ( " the %dth letter of alpha() is %s\n", n, C.alpha()[n-1] )
-print ( f" the constant has the value {C.pi():7.5}\n" )
+# https://docs.python.org/2/faq/design.html#why-can-t-i-use-an-assignment-in-an-expression
+n = 0
+with open("input.txt") as fo:
+ while True:
+  line = fo.readline()
+  if not line: break
+  listing.append(line)
+  n = n + 1
+fo.close()
+
+#[ print(x) for x in registry[0] ]
+#print( f" {n:d} lines in file\n" )
+
+[ checksum.append( x[ x.index('[')+1 : x.index(']') ] ) for x in listing ]
+[ is_real.append( sumcheck(listing[i],checksum[i]) ) for i in range(n) ]
+n1 = 0
+while ( not x.isdigit()
+sum( [ int(x[n1:n2]) for i in range(n) for x in listing if is_real[i] ] )
+
+
+
+#n = 15
+#print ( f" the {n:2d}th letter of alpha() is {C.alpha()[n-1]!r}\n" )
+#printf ( " the %dth letter of alpha() is %s\n", n, C.alpha()[n-1] )
+#print ( f" the constant has the value {C.pi():7.5}\n" )
 
