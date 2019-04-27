3d1c249f4fcd849a1d9f3a9140f7e9e28f71bdcd
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-04-27 10:06:14 -0700

    python decrypter broken

--- 2016/04/two.py
+++ 2016/04/two.py
@@ -1,18 +1,22 @@
 # v >= 3.6 for f-strings
+
+# C printf
 import sys
 def printf(format, *args):
     sys.stdout.write(format % args)
 
-# constants
+# python does not have constants --- "Just don't change it."
+# https://stackoverflow.com/questions/2682745/how-do-i-create-a-constant-in-python/2682752
 class C():
-    def pi():
-        return 3.1415926535897932384626433832795028841971
     def alpha():
         return "abcdefghijklmnopqrstuvwxyz"
 
+# not used, but useful for comparison to fortran and C
+# the python built-in .count is likely recursive
 def counter ( ch, string ):
     return sum( [ x is ch for x in string ] )
 
+# checksum ruleset
 def sumcheck ( x, y ):
     counts = [ x.count(ch) for ch in y ]
     if 0 in counts: return False
@@ -26,6 +30,16 @@ def sumcheck ( x, y ):
             ): continue
         else: return False
     return True
+
+def decrypter ( r ):
+    s = ""
+    rotate = r[0] % len( C.alpha() )
+    for i in range( len( r[1] ) ):
+        if r[1][i] == '-': s = s + ' '
+        else:
+            if C.alpha().index(r[1][i]) + rotate < len( C.alpha() ): s = s + C.alpha()[rotate]
+            else: s = s + C.alpha()[ rotate-len(C.alpha()) ] 
+    return s
 
 # poor man's struct
 #  The following mutable lists will be the members
@@ -64,6 +78,7 @@ fo.close()
 [ checksum.append( x[ x.index('[')+1 : x.index(']') ] ) for x in listing ]
 [ encrypted.append( x[ :x.index('[')-3 ] ) for x in listing ]
 [ is_real.append( sumcheck(encrypted[i],checksum[i]) ) for i in range(n) ]
+
 for i in range(n):
     s = listing[i]
     nums = [ ch.isdigit() for ch in s ]
@@ -72,7 +87,9 @@ for i in range(n):
         if ( nums[j] ):
             sub = sub + s[j]
     sector_id.append( int( sub ) )
-#[ print( listing[i], sector_id[i], is_real[i] ) for i in range(n) ]
+
+[ decrypted.append( decrypter([sector_id[i],encrypted[i]]) ) for i in range( len(listing) ) ]
+[ print( sector_id[i], decrypted[i] ) for i in range(n) ]
 print( f" total of real sector ids = {sum( [ sector_id[i] for i in range(n) if is_real[i] ] ):d}" )
 
 #n = 15
