9e3f5b852fc2f94cdbd12fc05cbc348c3482b092
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-05-02 14:21:36 -0700

    file io in lisp

--- 2016/04/two.lisp
+++ 2016/04/two.lisp
@@ -2,6 +2,8 @@
   (when (<= min max)
     (cons min (range (+ min step) max step))))
 
+(print (range 0 8))
+
 (defun counter (ch str)
   (let ((i 0))
   (loop for j from 0 to (- (length str) 1) do
@@ -13,6 +15,19 @@
 
 (counter "e" "the change has come she's under my thumb")
 
-;; figure out file i/o
-;;  http://www.gigamonkeys.com/book/files-and-file-io.html
-;; use recursive cons to add lines to a list ?
+;; Does my read loop involve full traversal ?
+;;  https://stackoverflow.com/questions/13359025/adding-to-the-end-of-list-in-lisp
+;; should the push instead be a lambda that acts like the range function above?
+(setf registry (list))
+(let ((fp (open "input.txt" :if-does-not-exist nil)))
+   (when fp
+      (loop for line = (read-line fp nil)
+      
+      while line do (push line registry))
+      (close fp)
+   )
+)
+(setf registry (reverse registry))
+
+(loop for s in registry do
+  (format t "~& ~a~%" s))
