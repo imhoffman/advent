f867a3953666b71a94e397a97fa9bf552b0cc1b6
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-05-08 21:35:32 -0700

    more scheme

--- 2016/04/two.scm
+++ 2016/04/two.scm
@@ -1,6 +1,10 @@
-(define registry '())
+(define registry (list))
 
 (define file (open-input-file "input.txt"))
 (do ((line (read-line file) (read-line file))) ((eof-object? line))
-        (append registry line))
+        (set! registry (append registry (list line))))
+
+(for-each
+ (lambda (s) (display s) (newline)) registry)
+
 
