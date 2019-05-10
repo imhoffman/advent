72a9e13cd2ca760d2c72012737bd72c763fb14a8
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-05-09 23:22:32 -0700

    scheme strings

--- 2016/04/two.scm
+++ 2016/04/two.scm
@@ -4,7 +4,5 @@
 (do ((line (read-line file) (read-line file))) ((eof-object? line))
         (set! registry (append registry (list line))))
 
-(for-each
- (lambda (s) (display s) (newline)) registry)
-
+(for-each (lambda (s) (display (substring s (string-find-next-char-in-set s (char-set #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\0)) (string-find-next-char s #\[)))(newline)) registry)
 
