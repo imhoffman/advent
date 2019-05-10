6749c833242c3bcfe5c89c356660324132b083da
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-05-09 23:27:05 -0700

    scheme numeric charset

--- 2016/04/two.scm
+++ 2016/04/two.scm
@@ -4,5 +4,8 @@
 (do ((line (read-line file) (read-line file))) ((eof-object? line))
         (set! registry (append registry (list line))))
 
-(for-each (lambda (s) (display (substring s (string-find-next-char-in-set s (char-set #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\0)) (string-find-next-char s #\[)))(newline)) registry)
+;(define nums (char-set #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\0))
+;(for-each (lambda (s) (display (substring s (string-find-next-char-in-set s nums) (string-find-next-char s #\[)))(newline)) registry)
+
+(for-each (lambda (s) (display (substring s (string-find-next-char-in-set s char-set:numeric) (string-find-next-char s #\[)))(newline)) registry)
 
