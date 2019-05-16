bb0ada55c82b60a5126c8f47edbd8f47974c4913
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-05-16 13:55:49 -0700

    more go

--- 2016/04/go/src/two/two.go
+++ 2016/04/go/src/two/two.go
@@ -7,31 +7,42 @@ import (
 	"os"
 )
 
-// file read
-//  https://golang.org/pkg/bufio/#Scanner
-//  https://stackoverflow.com/questions/8757389/reading-file-line-by-line-in-go
-func reader ( Nrooms *int ) {
+const max_lines int = 16384
+
+//  file read
+//   https://golang.org/pkg/bufio/#Scanner
+//   https://stackoverflow.com/questions/8757389/reading-file-line-by-line-in-go
+//  OMG!  from
+//   https://golang.org/doc/effective_go.html#data
+//   Note that, unlike in C, it's perfectly OK to return the address of a local variable; the storage associated with the variable survives after the function returns.
+func reader () []string {
     var i int = 0
+    temp := make( []string, max_lines )
 
     f, err := os.Open("input.txt")
     if err != nil { log.Fatal(err) }
     defer f.Close()
     s := bufio.NewScanner(f)
     for s.Scan() {
-      fmt.Println(s.Text())
+      temp[i] = s.Text()
       i++
     }
     if err := s.Err(); err != nil { log.Fatal(err) }
 
-    *Nrooms = i
-    return
+    //  https://blog.golang.org/go-slices-usage-and-internals
+    registry := make( []string, i )
+    copy( registry, temp )
+    return registry
 }
 
+
+//  main program
 func main () {
-    var Nrooms int
+    //var Nrooms int
 
-    reader ( &Nrooms )
+    r := reader()
+    fmt.Printf(" %v\n", r )
 
-    fmt.Printf("\n read %d lines\n\n", Nrooms)
+    fmt.Printf("\n read %d lines\n\n", len(r) )
 
 }
