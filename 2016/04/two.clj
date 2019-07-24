b041599ccb9537bcf7e69bb2fa455842a79f89c1
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-07-24 14:15:15 -0700

    clojure constants

--- 2016/04/two.clj
+++ 2016/04/two.clj
@@ -1,3 +1,9 @@
+(def constants {
+   :alpha "abcdefghijklmonpqrstuvwxyz",
+   :nums "0123456789",
+   :max_lines 8192
+   } )
+
 (def registry
   (with-open [f (clojure.java.io/reader "input.txt")]
     (reduce conj () (line-seq f))))
