bf79f1a59e6016234337b98039002084fbfae2f4
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-07-27 15:36:03 -0700

    begin clojure sumcheck

--- 2016/04/two.clj
+++ 2016/04/two.clj
@@ -1,10 +1,12 @@
 (require '[clojure.string :as str])
 
+;;  https://clojure.org/guides/learn/hashed_colls#_looking_up_by_key
 (def constants {
    :alpha "abcdefghijklmonpqrstuvwxyz",
    :nums "0123456789",
    } )
 
+;;  https://clojuredocs.org/clojure.core/line-seq
 (def registry
   (with-open [f (clojure.java.io/reader "input.txt")]
     (reduce conj () (line-seq f))))
@@ -14,6 +16,7 @@
 ;;  using `reduce` rather than a list comprehension allows the use of `reduced` to bail out
 ;;  rather than testing a mutable in a `while` or using a CL-like `return-from`
 ;;  and, `reduce` seems to send in two arguments, so I'm catching both `a` and `b` but only using `b`
+
 (defn first-digit [s]
   (reduce
     (fn [a b]
@@ -30,9 +33,35 @@
 
 (defn get-id [s] (Integer/parseInt (subs s (first-digit s) (+ 1 (last-digit s)))))
 
+;;
+;; substring retrievals utilities
+;;
 (defn get-checksum [s]
   (subs s
         (+ 1 (str/index-of s \[))
         (str/index-of s \])))
+
+(defn encrypted [s]
+  (subs s 0 (- (first-digit s) 1)))
+
+;;
+;; checksum verification
+;;
+;;  the ruleset
+(defn ruleset [checksum freqs strng]
+  true)
+
+;; parser and impossibility filter
+;;  https://clojuredocs.org/clojure.core/frequencies
+(defn sumcheck [s]
+  (let [checksum (get-checksum s)
+        strng    (encrypted s)
+        freqs    (frequencies strng)]
+  (if
+   (and
+    (reduce (fn [a b] (and a b)) (for [c (seq checksum)] (contains? freqs c)))
+    (ruleset checksum freqs strng))
+   true false)))
+
 
 
