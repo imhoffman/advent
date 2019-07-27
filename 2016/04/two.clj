ac4183d58c35f5940b224cb79207944302d572e2
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-07-26 23:45:00 -0700

    working on clojure

--- 2016/04/two.clj
+++ 2016/04/two.clj
@@ -1,3 +1,5 @@
+(require '[clojure.string :as str])
+
 (def constants {
    :alpha "abcdefghijklmonpqrstuvwxyz",
    :nums "0123456789",
@@ -9,3 +11,11 @@
     (reduce conj () (line-seq f))))
 
 (println (count registry))
+
+(def total 0)
+
+(doseq [s registry]
+  (doseq [a s]
+    (
+
+(doseq [s strg] (if (> (or (str/index-of nums s) -2) -1) (do (def id (Integer/parseInt (str s))) (println id))))
