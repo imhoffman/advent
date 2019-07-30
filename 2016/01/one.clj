ea5b3f9e74ff89d581f24abb98cfa0464e88fe33
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-07-29 22:57:48 -0700

    done with part one

--- 2016/01/one.clj
+++ 2016/01/one.clj
@@ -17,3 +17,31 @@
 
 ;(println (first dirvec) (last dirvec))
 
+;;  the `rest` of a str is a seq which has to be `join`ed before `parseInt`
+(defn numb [s] (Integer/parseInt (apply str (rest s))))
+
+(defn walker [v]
+  (loop [i 0    ; vector element
+         x 0    ; latitude (N +)
+         y 0    ; longitude (E +)
+         b 0]   ; North 0, West 1, South 2, East 3
+    (let [d (get v i)]
+    (if (= i (count v))    ; arrive here after `inc`
+      (+ x y)
+      (if (= (first d) \L)
+         (if (= b 0) (recur (inc i) (- x (numb d)) y              1)
+         (if (= b 1) (recur (inc i) x              (- y (numb d)) 2)
+         (if (= b 2) (recur (inc i) (+ x (numb d)) y              3)
+         (if (= b 3) (recur (inc i) x              (+ y (numb d)) 0)))))
+
+         (if (= b 0) (recur (inc i) (+ x (numb d)) y              3)
+         (if (= b 1) (recur (inc i) x              (+ y (numb d)) 0)
+         (if (= b 2) (recur (inc i) (- x (numb d)) y              1)
+         (if (= b 3) (recur (inc i) x              (- y (numb d)) 2))))))))))
+
+;(defn walker [v]
+;  (reduce + (for [d v]
+;    (fn [d]
+
+(println (walker dirvec))
+
