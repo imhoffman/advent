e91fe8c4f517dda12847130107dc2903bc32d579
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-07-29 22:09:50 -0700

    finished csv parser

--- 2016/01/one.clj
+++ 2016/01/one.clj
@@ -1,26 +1,19 @@
 (require '[clojure.string :as str])
 
-;;  cheat by adding trailing comma
+;;  file I/O
+;;   cheat by adding trailing comma
 (def directions (apply str (str/trim-newline (slurp "input.txt")) ","))
 
+;;  recursive csv parser to load vector of instructions
+;;   use vector b/c conj goes on tail and b/c order matters
+(def dirvec
+  (loop [a     directions
+         accum [] ]
+    (let [j (str/index-of a \,)    ; needs final comma added to slurp
+          r (subs a 0 j)]
+    (if (not (str/index-of a \space))
+      (conj accum r)               ; base case ... add final entry and return
+      (recur (subs a (+ j 2)) (conj accum r))))))
 
-;;  recursive function to obtain vector of directions
-;(conj []
-;  (loop [i 0
-;         j 0]
-;    (let [s (subs directions i)
-;          b ""]
-;    (for [c s]
-;      (if (not (= c \,))
-;        (recur i (+ 1 j))
-;        (do (subs s 0 j) (recur (+ 1 i) j)))))))
-
-(loop [a     directions
-       i     0
-       accum []]
-  (let [j (str/index-of a \,)
-        r (subs a 0 j)]
-  (if (< 3 (- j (count a)))     ; b/c r=-1 will miss the last one...
-    accum
-    (recur (subs s (+ j 1)) (+ i j 1) (conj accum r)))))
+;(println (first dirvec) (last dirvec))
 
