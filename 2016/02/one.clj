4eeda45707fdae123fd329ab5107b5664ab66dd2
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-08-01 07:53:46 -0700

    need to start from previous digit

--- 2016/02/one.clj
+++ 2016/02/one.clj
@@ -6,16 +6,33 @@
 ;(println " this should be 6:" (aget pad 1 2))
 
 (defn combo [s]
-  (for [r s]
-    (let [a (seq r)
-          i 1       ; horizontal index on keypad
-          j 1]      ; vertical index   start on "5" at 1,1
-    (for [p a]
-      (case p
-        \U (println "read a U")
-        \D (println "read a D")
-        \L (println "read a L")
-        \R (println "read a R"))))))
+  (let [mi (- (alength (aget pad 0)) 1)     ; max index dimensions on keypad
+        mj (- (alength pad) 1)]
+  (loop [r s
+         fi 1
+         fj 1]
+    (let [b (first r)
+          c (rest r)]
+    (loop [a (seq b)
+           i fi       ; "vertical" index on keypad
+           j fj]      ; "horizontal" index   start on "5" at 1,1
+      (println " from:" (aget pad i j) " do:" a)
+      (let [d (first a)
+            f (rest a)]
+      (case d
+        \U (let [ni (if (= i 0) i (dec i))
+                 nj j]
+             (if (empty? f) (print (aget pad ni nj)) (recur f ni nj)))
+        \D (let [ni (if (= i mi) i (inc i))
+                 nj j]
+             (if (empty? f) (print (aget pad ni nj)) (recur f ni nj)))
+        \L (let [ni i
+                 nj (if (= j 0) j (dec j))]
+             (if (empty? f) (print (aget pad ni nj)) (recur f ni nj)))
+        \R (let [ni i
+                 nj (if (= j mj) j (inc j))]
+             (if (empty? f) (print (aget pad ni nj)) (recur f ni nj))))))
+    (if (not (empty? c)) (recur c fi fj))))))    ;; this needs to be ni, nj ... in scope
 
 ;;
 ;;  main program
@@ -27,6 +44,7 @@
     (reduce conj () (line-seq f))))
 (println " number of lines read from input file:" (count document))
 
-(println (combo document))
+(combo (reverse document)) (println)
+;(println (combo (reverse document)))             ; `reverse` b/c `conj ()` for reading file
 
 
