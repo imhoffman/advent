fe8270b840fe094f5be7f70173ab181604cd4d04
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-08-01 16:22:40 -0700

    done with part one

--- 2016/02/one.clj
+++ 2016/02/one.clj
@@ -3,24 +3,18 @@
 ;;  https://clojuredocs.org/clojure.core/to-array-2d
 (def pad (to-array-2d [ [1 2 3] [4 5 6] [7 8 9] ]))
 
-;(println " this should be 6:" (aget pad 1 2))
 
-(defn combo [s]
-  (let [mi (- (alength (aget pad 0)) 1)     ; max index dimensions on keypad
+;;  part one ruleset
+;;   internal functionality of recursive `combo`
+(defn combo-line [b ii ij]
+  (let [mi (- (alength (aget pad 0)) 1)    ; max index dimensions on keypad
         mj (- (alength pad) 1)]
-  (loop [r s        ; loop through lines of instructions
-         fi 1       ; "vertical" index on keypad
-         fj 1]      ; "horizontal" index   start on "5" at 1,1
-    (let [b (first r)
-          c (rest r)]
-    ((fn [g] (if (empty? c) (print (first g)) (recur c (first (rest g)) (last g))))
-    ;((fn [g1 g2 g3] (if (empty? c) (print g1) (recur c g2 g3)))
-    (loop [a (seq b)  ; loop through instructions in a line
-           i fi
-           j fj]
-      ;(println " from:" (aget pad i j) " do:" a)
+    (loop [a (seq b)                       ; loop through instructions in a line
+           i ii
+           j ij]
       (let [d (first a)
             f (rest a)]
+      ;(println " about to move" d "from" (aget pad i j))
       (case d
         \U (let [ni (if (= i 0) i (dec i))
                  nj j]
@@ -33,8 +27,18 @@
              (if (empty? f) (list (aget pad ni nj) ni nj) (recur f ni nj)))
         \R (let [ni i
                  nj (if (= j mj) j (inc j))]
-             (if (empty? f) (list (aget pad ni nj) ni nj) (recur f ni nj)))))))))))
-    ;(if (not (empty? c)) (recur c fi fj))))))    ;; this needs to be ni, nj ... in scope
+             (if (empty? f) (list (aget pad ni nj) ni nj) (recur f ni nj))))))))
+
+
+;;   prints the numbers in the combo one at a time 
+(defn combo [s i j]
+  (let [b (first s)            ; loop through lines of instructions
+        c (rest s)]
+  ;(println " considering line:" b)
+  (let [g (combo-line b i j)]
+    (println (first g))
+    (if (not (empty? c)) (combo c (first (rest g)) (last g))))))
+
 
 ;;
 ;;  main program
@@ -46,7 +50,8 @@
     (reduce conj () (line-seq f))))
 (println " number of lines read from input file:" (count document))
 
-;(combo (reverse document)) (println)
-(println (combo (reverse document)))             ; `reverse` b/c `conj ()` for reading file
+     ; "vertical" index on keypad
+     ; "horizontal" index   start on "5" at 1,1
+(println (combo (reverse document) 1 1))             ; `reverse` b/c `conj ()` for reading file
 
 
