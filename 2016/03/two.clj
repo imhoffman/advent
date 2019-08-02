ae774de54db0b51fe7f8553add57e78837072002
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-08-02 10:20:37 -0700

    more part two; need 2d array addressing

--- 2016/03/two.clj
+++ 2016/03/two.clj
@@ -7,19 +7,31 @@
 
 ;;  predicate for valid triples
 (defn valid? [t]
+  (println "in valid")
   (assert (= 3 (count t)))
   (let [a (first t)
         b (first (rest t))
         c (last t)]
+    (println " valid? is considering:" a b c)
     (and (> (+ b c) a)
          (> (+ a c) b)
          (> (+ b a) c))))
 
 ;;  part two column-bundler
+;user=> (for [i (range 0 16 3)] ((fn [j] (list (+ j 1) (+ j 2) (+ j 3))) i))
+;((1 2 3) (4 5 6) (7 8 9) (10 11 12) (13 14 15) (16 17 18))
+;user=> (for [a (for [i (range 0 16 3)] ((fn [j] (list (+ j 1) (+ j 2) (+ j 3))) i))] (first a))
+;(1 4 7 10 13 16)
+;(user=> (for [a (for [i (range 0 16 3)] ((fn [j] (vector (+ j 1) (+ j 2) (+ j 3))) i))] (first a))
+;(1 4 7 10 13 16)
 (defn bundler [v]
   (assert (= 0 (mod (count v) 3)))
-  (list
-  (loop [i 0]
+  (println " passed to bundler:" v)
+  (println " a sample output list:"
+           (let [i 3]
+           (list (get v i 0) (get v (+ i 1) 0) (get v (+ i 2) 0)))) ; HERE! need to pull out vector, then address it b/c each line is already a vector...it is not a 2d array...perhaps `let` a 2d array ?
+  (for [i (range 0 (mod (count v) 3) 3)]
+    (reduce conj ()
   ;(doseq [i (range 0 (- (mod (count v) 3) 3) 3)]
     ;(let [t1 (list (get v i 0) (get v (+ i 1) 0) (get v (+ i 2) 0))
     ;      t2 (list (get v i 1) (get v (+ i 1) 1) (get v (+ i 2) 1))
@@ -27,17 +39,17 @@
     ;  t1 t2 t3
       (list (get v i 0) (get v (+ i 1) 0) (get v (+ i 2) 0))
       (list (get v i 1) (get v (+ i 1) 1) (get v (+ i 2) 1))
-      (list (get v i 2) (get v (+ i 1) 2) (get v (+ i 2) 2))
-      (if (not (< i (mod (count v) 3))) (recur (+ i 3))))))
+      (list (get v i 2) (get v (+ i 1) 2) (get v (+ i 2) 2)))))
+      ;(if (< (+ i 3) (count v)) (recur (+ i 3))))))
 
 ;;
 ;;  main program
 ;;
 ;;   file I/O
 ;;   https://clojuredocs.org/clojure.core/line-seq
-(def input
+(def input (reverse             ; conj appends to front of list
   (with-open [f (clojure.java.io/reader "input.txt")]
-    (reduce conj () (line-seq f))))
+    (reduce conj () (line-seq f)))))
 (println " number of lines read from input file:" (count input))
 
 ;(println " number of valid listings:" (get (frequencies (for [u (parser input)] (valid? u))) true))
@@ -45,4 +57,10 @@
          (get (frequencies
                 (for [u (bundler (parser input))]
                   (valid? u))) true))
+
+(println " output from parser:")
+(println (parser input))
+
+(println " output from bundler:")
+(println (bundler (parser input)))
 
