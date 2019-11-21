f009ae986c31e8d098df556e3f0d6b1fe017b2e7
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-21 11:19:26 -0800

    more day 7

--- 2016/07/one.clj
+++ 2016/07/one.clj
@@ -1,13 +1,21 @@
 (require '[clojure.string :as str])
 
+;; parse substrings from \[ and \] before calling this
+(defn has-palindrome? [s]
+  (let [L 4]
+    (if (subs s 0 L) true false)))
 
 ;;
 ;;  main program
 ;;
 ;;   file I/O
 ;;   https://clojuredocs.org/clojure.core/line-seq
-(def registry
+;;   NB: lists get appended at the front; conj to a vec if you care
+(def input
   (with-open [f (clojure.java.io/reader "puzzle.txt")]
     (reduce conj () (line-seq f))))
-(println (count registry))
+(println "Input file has" (count input) "lines.")
+
+(let [s (str/split (first input) #"")]
+  (println (first input) (first s) (last s)))
 
