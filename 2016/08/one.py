04a2d3cf59a9fb0c39c0345077bee28748948041
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-25 10:58:43 -0800

    working on modulus wrap for rotate

--- 2016/08/one.py
+++ 2016/08/one.py
@@ -9,21 +9,36 @@ class screen:
 
   def command ( self, r ):
     d = self.state
+    print( r )
     if r.split(sep=' ')[0] == 'rect':
         x,y = [ int(n) for n in r.split(sep=' ')[1].split(sep='x') ]
         for i in range(x):
             for j in range(y):
-                d[j][i] = '#'
+                d[j][i] = 1
     elif r.split(sep=' ')[0] == 'rotate':
-        print( " rotate command encountered\n" )
+        e = r.find('=')+1
+        if r.split(sep=' ')[1] == 'row':
+            y = int( r[ e:r.find(' ',e) ] )
+            n = int( r[ r.find('by')+2: ] )
+            #print( " rotate row command encountered: row %d by %d\n" % (y, n) )
+            temp = d[y][-n:]
+            for i in range( len(d[0])-n-1 ):
+                d[y][i+1] = d[y][i]
+            # something like d[y][0] = temp
+        elif r.split(sep=' ')[1] == 'column':
+            x = int( r[ e:r.find(' ',e) ] )
+            m = int( r[ r.find('by')+2: ] )
+            #print( " rotate column command encountered: column %d by %d\n" % (x, m) )
+            temp = d[-m:][x]
+            for j in range( len(d)-m-1 ):
+                d[j+m][x] = d[j][x]
+            #something like d[0][x] = temp
+        else:
+            print( " problem reading rotate command\n" )
     else:
         print( " command unknown\n" )
     return
 
-  def test_point ( self, x0, y0 ):
-    d = self.state
-    d[y0][x0] = '#'
-    return
 
   def render ( self ):
     length = self.length
@@ -54,7 +69,8 @@ with open("puzzle.txt") as fo:
 print( " read %d lines from input file\n" % (n) )
 
 # instantiate a screen object with an internally-tracked state
-display = screen( 50, 6 )
+display = screen( 7, 3 )
+#display = screen( 50, 6 )
 
 for s in listing:
     display.command( s )
