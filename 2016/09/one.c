dbbab480586bee1cdf2011f1c922aa614c8d899e
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-28 15:08:44 -0800

    re-write C one without stat

--- 2016/09/one.c
+++ 2016/09/one.c
@@ -2,31 +2,59 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<sys/stat.h>
-//  the stat library will only work on POSIX; perhaps fseek to SEEK_END elsewhere
+
+#define MAXCHARS 65536
+
+//  file-reader subroutine to determine dynamic lengths
+void reader( FILE *f, int *n, char file_contents[] ) {
+  char buffer[MAXCHARS] = { '\0' };
+  int num_lines = 0;
+  char *qnull;
+  size_t file_length;
+
+  while ( fgets( buffer, MAXCHARS, f ) != NULL ) {
+    strncpy( file_contents, buffer, MAXCHARS );
+    num_lines = num_lines + 1;
+  }
+
+  if ( num_lines > 1 ) {
+	  printf( "\n PROBLEM: more than one line in file\n" );
+	  *n = -1;
+	  return;
+  }
+
+  qnull = strchr( file_contents, '\0' );
+  file_length = qnull - &file_contents[0];   // difference of size_t's
+  *n =(int) file_length;
+
+  return;
+}
 
 
 //
 // main program
 //
 int main( int argc, char *argv[] ) {
- FILE* fp;
- int compressed_length;
+  FILE* fp;
+  int nchars;
 
- // https://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
- struct stat st;
- stat( "puzzle.txt", &st );
- compressed_length = st.st_size;
+  //  this would work---instead of the following two lines---but
+  //  only this way can be immediately freed
+  //char temp[MAXCHARS] = { '\0' };
+  
+  char *temp;
+  temp =(char *) malloc( MAXCHARS * sizeof( char ) );
 
- char *compressed = malloc( (compressed_length+1) * sizeof( char ) );
+  fp = fopen("puzzle.txt","r");
+  reader( fp, &nchars, temp);
+  fclose(fp);
 
- fp = fopen("puzzle.txt","r");
- fgets( compressed, compressed_length, fp );
- fclose(fp);
- compressed[compressed_length+1] = '\0';
+  char *compressed_string = malloc( (nchars+1) * sizeof( char ) );
+  strncpy( compressed_string, temp, nchars );
+  free( temp );     // free the huge temporary file-read buffer
+  compressed_string[nchars+1] = '\0';    // in this needed ?
 
- printf( "\n %s\n", compressed );
+  printf( "\n %s\n", compressed_string );
 
- free( compressed );
- return 0;
+  return 0;
 }
