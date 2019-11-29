2c10f06c86d0aea4130964930f6176178594571f
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-29 14:01:03 -0800

    simplify clojure two with slurp

--- 2015/01/two.clj
+++ 2015/01/two.clj
@@ -2,13 +2,11 @@
 ;;  main program
 ;;
 ;;   file I/O
-;;   https://clojuredocs.org/clojure.core/line-seq
-(def input
-  (with-open [f (clojure.java.io/reader "puzzle.txt")]
-    (first (reduce conj () (line-seq f)))))      ; for a single-line input
+;;    for a single-line input
+(def input (slurp "puzzle.txt"))
 
-;;  I have applied Chuck's answer here
-;;   https://stackoverflow.com/questions/7491360/how-do-you-return-from-a-function-early-in-clojure
+;;  https://stackoverflow.com/questions/7491360/how-do-you-return-from-a-function-early-in-clojure
+;;   I have applied Chuck's answer here
 ((fn [directions index floor]
    (if (= floor -1) (println "\n basement encountered at index" (dec index) "\n")
      (if (= (first directions) \()
