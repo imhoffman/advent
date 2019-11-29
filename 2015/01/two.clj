f7ddb47cf49d7796ddf2ec6b5570e634b35e9cd2
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-29 15:05:20 -0800

    modularize by removing lambda from clojure two

--- 2015/01/two.clj
+++ 2015/01/two.clj
@@ -1,3 +1,12 @@
+
+;;  https://stackoverflow.com/questions/7491360/how-do-you-return-from-a-function-early-in-clojure
+;;   I have applied Chuck's answer here
+(defn basement-finder [directions index floor]
+   (if (= floor -1) (dec index)    ;; return one fewer b/c of the recur inc
+     (if (= (first directions) \()
+       (recur (rest directions) (inc index) (inc floor))
+       (recur (rest directions) (inc index) (dec floor)))))
+
 ;;
 ;;  main program
 ;;
@@ -5,12 +14,5 @@
 ;;    for a single-line input
 (def input (slurp "puzzle.txt"))
 
-;;  https://stackoverflow.com/questions/7491360/how-do-you-return-from-a-function-early-in-clojure
-;;   I have applied Chuck's answer here
-((fn [directions index floor]
-   (if (= floor -1) (println "\n basement encountered at index" (dec index) "\n")
-     (if (= (first directions) \()
-       (recur (rest directions) (inc index) (inc floor))
-       (recur (rest directions) (inc index) (dec floor))))
-   ) input 1 0)
+(println "\n basement encountered at index" (basement-finder input 1 0) "\n")
 
