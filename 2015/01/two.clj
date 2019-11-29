fb32d2a987e594ce12ff55778fb73e577097ffb0
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-29 13:56:34 -0800

    done with clojure two

--- 2015/01/two.clj
+++ 2015/01/two.clj
@@ -7,10 +7,12 @@
   (with-open [f (clojure.java.io/reader "puzzle.txt")]
     (first (reduce conj () (line-seq f)))))      ; for a single-line input
 
+;;  I have applied Chuck's answer here
+;;   https://stackoverflow.com/questions/7491360/how-do-you-return-from-a-function-early-in-clojure
 ((fn [directions index floor]
-   (if (= floor -1) (println "\n basement encountered at index" (dec index)))
-   (if (= (first directions) \()
-     (recur (rest directions) (inc index) (inc floor))
-     (recur (rest directions) (inc index) (dec floor)))
+   (if (= floor -1) (println "\n basement encountered at index" (dec index) "\n")
+     (if (= (first directions) \()
+       (recur (rest directions) (inc index) (inc floor))
+       (recur (rest directions) (inc index) (dec floor))))
    ) input 1 0)
 
