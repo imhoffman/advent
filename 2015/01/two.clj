f334b2851a216c6fb8ef073809b69e627bd80d5e
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-29 12:07:43 -0800

    clojure two is working, but looping forever

--- 2015/01/two.clj
+++ 2015/01/two.clj
@@ -9,18 +9,10 @@
   (with-open [f (clojure.java.io/reader "puzzle.txt")]
     (first (reduce conj () (line-seq f)))))      ; for a single-line input
 
-;; don't use loop; perhaps `reduce` a sum
-
-(loop [index 1
-       floor 0
-       c (first input)]
-  (let [s (first (rest input))]
-    (if (= floor -1)
-      (println "\n encountered floor -1 at index" index))
-    (if (and (= c "(") (not (str/blank? s)))
-      (recur (inc index) (inc floor) s)
-      (recur (inc index) (dec floor) s))))
-
-(let [freqs (frequencies input)]
-  (println " final floor:" (- (get freqs \() (get freqs \)))))
+((fn [directions index floor]
+   (if (= floor -1) (println "\n basement encountered at floor" (dec index)))
+   (if (= (first directions) \()
+     (recur (rest directions) (inc index) (inc floor))
+     (recur (rest directions) (inc index) (dec floor)))
+   ) input 1 0)
 
