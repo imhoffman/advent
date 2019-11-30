09a8f9aea3029b58a8258da9738aebb8ba188802
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-29 20:18:14 -0800

    done with C one

--- 2015/03/one.c
+++ 2015/03/one.c
@@ -1,50 +1,75 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
 
-#define MAXMOVES 1048576
+#define MAXMOVES 16384
 
+// subroutine for updating the current location
 void mover ( const char current_move, int *x, int *y ) {
   switch ( current_move ) {
-	  case '>': *x = *x + 1; break;
-	  case '^': *y = *y + 1; break;
-	  case '<': *x = *x - 1; break;
-	  case 'v': *y = *y - 1; break;
-	  default: printf( "\n :(\n" );
+    case '>': *x = *x + 1; break;
+    case '^': *y = *y + 1; break;
+    case '<': *x = *x - 1; break;
+    case 'v': *y = *y - 1; break;
+    default: printf( "\n :(\n" );   // error checker...should not happen
   }
   return;
 }
 
-
+//
+//  main program
+//
 int main ( void ) {
-  char buffer[MAXMOVES] = { '\0' };
+  char buffer[MAXMOVES];    // fgets will terminate with a null
   FILE *fp;
   char *address_of_null;
   size_t difference_of_memory_locations;
-  int number_of_moves;
+  int number_of_moves, number_of_houses_visited;
+  int x=0, y=0;                         // maybe an ordered-pair struct ?
+  bool match;
+  int houses[MAXMOVES][3] = { -9000 };  //  2D array: x, y, number_of_presents
+                                        //  initialize with negative presents
+					//  for identification later
 
+  //  file I/O
   fp = fopen( "input.txt", "r" );
   if ( fgets( buffer, MAXMOVES, fp ) != NULL ) {
     address_of_null = strchr( buffer, '\0' );
     difference_of_memory_locations = address_of_null - &buffer[0];
     number_of_moves =(int) difference_of_memory_locations;
   } else {
-    printf( " :(\n " );
+    printf( " :(\n " );    // error checking
     return 1;
   }
   fclose(fp);
 
-  //char moves[8193] = { '\0' };
-  //char moves[number_of_moves+1] = { '\0' };
-  //strncpy( moves, buffer, number_of_moves );
-
-  int x=0, y=0;    // maybe an ordered-pair struct ?
+  //  puzzle solution
+  //   loop over moves
   for ( int i = 0; i < number_of_moves; i++ ) {
-    mover( buffer[i], &x, &y );
-    printf( " move: %c, x: %d, y: %d\n", buffer[i], x, y );
+    mover( buffer[i], &x, &y );     // update location
+
+    //  look for a match to a previous visit
+    match = false;
+    for ( int j = 0; j < i; j++ ) { //  NB: j does not map to i
+      if ( houses[j][0] == x  &&  houses[j][1] == y ) {
+        houses[j][2] += 1;          // give them one more present
+	match = true;
+        break;
+      }
+    }
+    if ( !match ) {           // must be the first visit
+       houses[i][2] = 1;      // give them their first present
+       houses[i][0] = x; houses[i][1] = y;
+    }
   }
 
-  //printf( "\n number of moves read from file: %d\n\n", number_of_moves );
+  //  loop over houses and count the ones without the negative initializer
+  number_of_houses_visited = 0;
+  for ( int i = 0; i < number_of_moves; i++ ) {
+    if ( houses[i][2] > 0 ) { number_of_houses_visited += 1; }
+  }
+
+  printf( "\n number of houses visited: %d\n\n", number_of_houses_visited );
 
   return 0;
 }
