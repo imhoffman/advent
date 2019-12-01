1019edb460460466e04c8b35017d3a041e65cc35
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-11-30 17:12:32 -0800

    done with one

--- 2015/04/one.sh
+++ 2015/04/one.sh
@@ -2,5 +2,14 @@
 
 input=$1
 i=0
-echo -n $1$i | md5sum -
+#d="0000000af0000c9"
+until [[ $d =~ ^0{5,}.* ]]
+do
+	#echo Checking $input$i
+	d=$(echo -n $input$i | md5sum -)
+	((i+=1))
+done
+((i-=1))
+echo -n $input$i | md5sum -
+echo $input$i
 
