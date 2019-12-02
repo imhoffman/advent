58366dead1dd72e7a00f30f46a9f3f4c7338526f
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-02 07:59:29 -0800

    another round of brute force

--- 2016/05/two.sh
+++ 2016/05/two.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 input=$1
-i=5833678
+i=24090052
 matches=0
-until [[ $matches -ge 24 ]]
+until [[ $matches -ge 36 ]]
 do
 	if [[ $(echo -n $input$i | md5sum -) =~ ^0{5,}.* ]]
 	then
-		echo -n $input$i | md5sum -
-		echo $input$i >> output2.txt
+		echo $input$i >> output3.txt
+		echo -n $input$i | md5sum - >> output3.txt
 		((matches+=1))
 	fi
 	((i+=1))
