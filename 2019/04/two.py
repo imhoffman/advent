681be5fe07f75f1c12549f35afbf750e237cc300
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-03 23:29:37 -0800

    two: still no joy ... giving up for now

--- 2019/04/two.py
+++ 2019/04/two.py
@@ -21,12 +21,17 @@ def has_repeats ( string_of_digits, number_found ):
             return True
         else:
             return False
-    for i in range( len( string_of_digits )-1 ):
-        if int( string_of_digits[i] ) == int( string_of_digits[i+1] ):
-            j = int( string_of_digits[i] )
-            print( "\n found a", j )
-            number_found[j] += 1
-            return has_repeats( string_of_digits[1:], number_found )
+    elif len( string_of_digits ) == 2:
+        #if int( string_of_digits[0] ) == int( string_of_digits[1] ):
+        #    j = int( string_of_digits[0] )
+        #    number_found[j] += 1
+            return has_repeats( "Q", number_found )
+    else:
+        for i in range( len( string_of_digits )-1 ):
+            if int( string_of_digits[i] ) == int( string_of_digits[i+1] ):
+                j = int( string_of_digits[i] )
+                number_found[j] += 1
+                return has_repeats( string_of_digits[1:], number_found )
     #return False      # should never get here
 
 
@@ -36,7 +41,7 @@ def rules ( lower_bound, upper_bound ):
         candidate_digits = str( i )
         if not is_increasing( candidate_digits ):
             continue
-        if not has_repeats( candidate_digits, [0]*n ):
+        if not has_repeats( candidate_digits, [0]*10 ):
             continue
         list_of_possibilities.append( i )
     return list_of_possibilities
@@ -54,14 +59,15 @@ print( "\n read %d characters from input file\n" % ( len(puzzle) ) )
 lower_bound = int( puzzle[:6] )
 upper_bound = int( puzzle[7:] )
 
-i = 444466
-chars = str(i)
-print( has_repeats( chars , [0]*10 ) )
+#i = 444466
+#chars = str(i)
+#print( has_repeats( chars , [0]*10 ) )
 
-#possibilities = rules( lower_bound, upper_bound )
-#
-#print( possibilities )
-#print( "\n number of possible passwords: %d\n\n" % len( possibilities ) )
+possibilities = rules( lower_bound, upper_bound )
+
+print( possibilities )
+print( "\n number of possible passwords: %d\n\n" % len( possibilities ) )
 
 # 175 is too low
+# 303 is wrong
 
