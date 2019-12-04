b9482bf4c2c9d6bf9a653354ee37adbdac4f5b77
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-03 22:14:07 -0800

    trying different interpretation of rules concerning repeated

--- 2019/04/one.py
+++ 2019/04/one.py
@@ -15,14 +15,19 @@ def is_increasing ( string_of_digits ):
                 return is_increasing( string_of_digits[1:] )
 
 
-def has_repeats ( string_of_digits ):
+def has_one_repeated_value ( string_of_digits, have_one_match, existing_match ):
     if len( string_of_digits ) == 1:
-        return False
+        return have_one_match
     for i in range( len( string_of_digits )-1 ):
         if int( string_of_digits[i] ) == int( string_of_digits[i+1] ):
-            return True
+            current_match = string_of_digits[i]
+            if have_one_match and current_match != existing_match:
+                return False
+            else:
+                return has_one_repeated_value( string_of_digits[1:], True, current_match )
+
         else:
-            return has_repeats ( string_of_digits[1:] )
+            return has_one_repeated_value ( string_of_digits[1:], have_one_match, existing_match )
 
 
 def rules ( lower_bound, upper_bound ):
@@ -31,7 +36,7 @@ def rules ( lower_bound, upper_bound ):
         candidate_digits = str( i )
         if not is_increasing( candidate_digits ):
             continue
-        if not has_repeats( candidate_digits ):
+        if not has_one_repeated_value( candidate_digits, False, None ):
             continue
         list_of_possibilities.append( i )
     return list_of_possibilities
@@ -52,6 +57,8 @@ upper_bound = int( puzzle[7:] )
 
 possibilities = rules( lower_bound, upper_bound )
 
+print( possibilities )
 print( "\n number of possible passwords: %d\n\n" % len( possibilities ) )
 
+## 1820 too high
 
