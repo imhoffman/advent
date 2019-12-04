0d4db6f06fde2b78e0e0cc02b3209ef81e12ff3b
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-03 23:20:21 -0800

    range(0) = (0,0) ... double counting the last recur!

--- 2019/04/two.py
+++ 2019/04/two.py
@@ -24,6 +24,7 @@ def has_repeats ( string_of_digits, number_found ):
     for i in range( len( string_of_digits )-1 ):
         if int( string_of_digits[i] ) == int( string_of_digits[i+1] ):
             j = int( string_of_digits[i] )
+            print( "\n found a", j )
             number_found[j] += 1
             return has_repeats( string_of_digits[1:], number_found )
     #return False      # should never get here
@@ -35,7 +36,7 @@ def rules ( lower_bound, upper_bound ):
         candidate_digits = str( i )
         if not is_increasing( candidate_digits ):
             continue
-        if not has_repeats( candidate_digits, [0,0,0,0,0,0,0,0,0,0] ):
+        if not has_repeats( candidate_digits, [0]*n ):
             continue
         list_of_possibilities.append( i )
     return list_of_possibilities
@@ -53,10 +54,14 @@ print( "\n read %d characters from input file\n" % ( len(puzzle) ) )
 lower_bound = int( puzzle[:6] )
 upper_bound = int( puzzle[7:] )
 
-possibilities = rules( lower_bound, upper_bound )
+i = 444466
+chars = str(i)
+print( has_repeats( chars , [0]*10 ) )
 
-print( possibilities )
-print( "\n number of possible passwords: %d\n\n" % len( possibilities ) )
+#possibilities = rules( lower_bound, upper_bound )
+#
+#print( possibilities )
+#print( "\n number of possible passwords: %d\n\n" % len( possibilities ) )
 
 # 175 is too low
 
