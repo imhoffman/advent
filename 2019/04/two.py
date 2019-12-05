9fa4bdde35ee9ee267d43cef4ace2779bfca66fd
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-04 20:02:35 -0800

    done with python two---use iterables rather than indexing

--- 2019/04/two.py
+++ 2019/04/two.py
@@ -18,6 +18,7 @@ def is_increasing ( string_of_digits ):
             else:
                 return is_increasing( string_of_digits[1:] )
 
+
 #  recursive predicate for the "repeated" rule
 #   repeated_digits_found is an array of each digit's repeat count
 def has_repeats ( string_of_digits, repeated_digits_found ):
@@ -27,12 +28,12 @@ def has_repeats ( string_of_digits, repeated_digits_found ):
         else:
             return False
     else:
-        a, b = [ d[i][0] for d in [ string_of_digits[:-1], string_of_digits[1:] ] for i in (0,1) ]
+        a, b = [ d[0] for d in [ string_of_digits[:-1], string_of_digits[1:] ] ]
         if int( a ) == int( b ):
-            j = int( a )                      # use its value for indexing
+            j = int( a )                          # use its value for indexing
             repeated_digits_found[j] += 1
-            # recur with remainder of string
-            return has_repeats( string_of_digits[1:], repeated_digits_found )
+        # recur with remainder of string
+        return has_repeats( string_of_digits[1:], repeated_digits_found )
     #return False                                 # should never get here
 
 
@@ -51,20 +52,15 @@ def rules ( lower_bound, upper_bound ):
 ##
 ##  main program
 ##
-
 with open("puzzle.txt") as fo:
   puzzle = fo.readline().strip()
 
 print( "\n read %d characters from input file\n" % ( len(puzzle) ) )
 
-lower_bound = int( puzzle[:6] )
-upper_bound = int( puzzle[7:] )
+lower_bound, upper_bound = map( int, puzzle.split('-') )
 
 possibilities = rules( lower_bound, upper_bound )
 
-print( possibilities )
+#print( possibilities )
 print( "\n number of possible passwords: %d\n\n" % len( possibilities ) )
-
-# 175 is too low
-# 303 is wrong
 
