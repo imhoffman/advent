ec4fa12c0cdf051cf7f77e9c93bf0a54cb40b54b
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-05 11:55:13 -0800

    work on fortran two

--- 2019/04/two.f90
+++ 2019/04/two.f90
@@ -58,11 +58,42 @@
 
 
   !!  test for repetitions as per rules
-  recursive function has_repeat ( n ) result ( predicate )
-    integer, intent(in) :: n
-    logical             :: predicate
+  recursive function has_repeat ( as_integer ) result ( predicate )
+    integer, intent(in)        :: as_integer
+    logical                    :: predicate
+    character(:), allocatable  :: chars
+    integer, allocatable       :: as_integer_array(:)
+    integer                    :: repeat_counting_array(10)
+    integer                    :: i, j, num_chars, a, b
 
+    chars = as_char_array( as_integer )
+    num_chars = len( chars )
+    allocate( as_integer_array(num_chars) )
+
+    !!  terminal case: we have recurred down to one digit
+    if ( num_chars .eq. 1 ) then
+      do i = 1, 10
+        if ( repeat_counting_array(i) .eq. 1 ) then
+          predicate = .true.
+          goto 200
+        endif
+      enddo
+    else
+      do i = 1, num_chars-1
+        if ( as_integer_array(i) .eq. as_integer_array(i+1) ) then
+          j = as_integer_array(i)
+          repeat_counting_array(j) = repeat_counting_array(j) + 1
+          do j = num_chars
+          current_modulus = mod( as_integer, int( 10**num_chars ) )
+          integer_for_recur = ( as_integer - current_modulus )/10**num_chars
+          predicate = has_repeat
+      enddo
+    endif
+
+
+    deallocate( as_integer_array )
     predicate = .false.
+    200 continue
     return
   end function has_repeat
  end module subs
@@ -87,7 +118,7 @@
 
   !test = lower_bound
   test = 123456
-  write(6,*) is_increasing( test )
+  write(6,*) test, " is increasing? ", is_increasing( test )
 
   stop
  end program main
