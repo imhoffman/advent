b067079c84f25ddf11b5ca6cede5d84e110f5247
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-05 19:38:32 -0800

    migrate parameter interpretation to separate function

--- 2019/05/two.py
+++ 2019/05/two.py
@@ -23,37 +23,31 @@ def parse_opcode ( ABCDE ):
         A = 0
     return opcode, C, B, A
 
-
-def processor ( ram, ip ):
-    opcode, mode1, mode2, mode3 = parse_opcode( ram[ip] )
-    print( " ip:", ip, " opcode:", ram[ip], "  parsed opcode:", opcode, " modes:", mode1,mode2,mode3 )
-    if   opcode == 1:
-        if mode1:
+#  for fetching the appropriate value as per the instruction modalities
+def modal_parameters ( ram, ip, modes ):
+        if modes[0]:
             arg1 = ram[ip+1]
         else:
             arg1 = ram[ ram[ip+1] ]
-        if mode2:
+        if modes[1]:
             arg2 = ram[ip+2]
         else:
             arg2 = ram[ ram[ip+2] ]
         # "Parameters that an instruction writes to will never be in immediate mode."
-        assert mode3 == 0
+        assert modes[2] == 0
         arg3 = ram[ip+3]
-        ##  the operation
+        return arg1, arg2, arg3
+
+
+def processor ( ram, ip ):
+    opcode, mode1, mode2, mode3 = parse_opcode( ram[ip] )
+    #print( " ip:", ip, " opcode:", ram[ip], "  parsed opcode:", opcode, " modes:", mode1,mode2,mode3 )
+    if   opcode == 1:
+        arg1, arg2, arg3 = modal_parameters( ram, ip, (mode1, mode2, mode3) )
         ram[ arg3 ] = arg1 + arg2
         return ram, ip+4
     elif opcode == 2:
-        if mode1:
-            arg1 = ram[ip+1]
-        else:
-            arg1 = ram[ ram[ip+1] ]
-        if mode2:
-            arg2 = ram[ip+2]
-        else:
-            arg2 = ram[ ram[ip+2] ]
-        assert mode3 == 0
-        arg3 = ram[ip+3]
-        ##  the operation
+        arg1, arg2, arg3 = modal_parameters( ram, ip, (mode1, mode2, mode3) )
         ram[ arg3 ] = arg1 * arg2
         return ram, ip+4
     elif opcode == 3:
@@ -64,58 +58,26 @@ def processor ( ram, ip ):
         print( "\n The program has outputted: %d\n\n" % ram[ ram[ip+1] ] )
         return ram, ip+2
     elif opcode == 5:
-        if mode1:
-            arg1 = ram[ip+1]
-        else:
-            arg1 = ram[ ram[ip+1] ]
-        if mode2:
-            arg2 = ram[ip+2]
-        else:
-            arg2 = ram[ ram[ip+2] ]
+        arg1, arg2, arg3 = modal_parameters( ram, ip, (mode1, mode2, mode3) )
         if arg1:
             return ram, arg2
         else:
             return ram, ip+3
     elif opcode == 6:
-        if mode1:
-            arg1 = ram[ip+1]
-        else:
-            arg1 = ram[ ram[ip+1] ]
-        if mode2:
-            arg2 = ram[ip+2]
-        else:
-            arg2 = ram[ ram[ip+2] ]
+        arg1, arg2, arg3 = modal_parameters( ram, ip, (mode1, mode2, mode3) )
         if not arg1:
             return ram, arg2
         else:
             return ram, ip+3
     elif opcode == 7:
-        if mode1:
-            arg1 = ram[ip+1]
-        else:
-            arg1 = ram[ ram[ip+1] ]
-        if mode2:
-            arg2 = ram[ip+2]
-        else:
-            arg2 = ram[ ram[ip+2] ]
-        assert mode3 == 0
-        arg3 = ram[ip+3]
+        arg1, arg2, arg3 = modal_parameters( ram, ip, (mode1, mode2, mode3) )
         if arg1 < arg2:
             ram[ arg3 ] = 1
         else:
             ram[ arg3 ] = 0
         return ram, ip+4
     elif opcode == 8:
-        if mode1:
-            arg1 = ram[ip+1]
-        else:
-            arg1 = ram[ ram[ip+1] ]
-        if mode2:
-            arg2 = ram[ip+2]
-        else:
-            arg2 = ram[ ram[ip+2] ]
-        assert mode3 == 0
-        arg3 = ram[ip+3]
+        arg1, arg2, arg3 = modal_parameters( ram, ip, (mode1, mode2, mode3) )
         if arg1 == arg2:
             ram[ arg3 ] = 1
         else:
@@ -143,6 +105,4 @@ while ip != -1:
     program, ip = processor( program, ip)
 
 print( "\n the value at position 0 after halting is %d\n\n" % program[0] )
-
-# 224 is too low
 
