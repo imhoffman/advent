1a755d0c2dac22b1dc71bca37f75af42a55f0d45
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-07 21:35:22 -0800

    vector of layers, not a list

--- 2019/08/one.clj
+++ 2019/08/one.clj
@@ -16,17 +16,20 @@
     (str/trim (slurp f))))
 (println "Read" (count input) "puzzle characters from one line.")
 
+;;  parse individual layers from input into a vector
 (def layers
   ((fn [s accum]
     (if (str/blank? s)
       accum
       (let [r (subs s 0 (dec pixels_per_layer))]
         (recur (subs s pixels_per_layer) (conj accum r)))))
-  input ()))
+  input []))
 
+(println (get layers 2))
 
-(doseq [layer layers]
-  (println layer))
-
+;;  part one ruleset
+;(def part-one
+;  (for [layer layers]
+;    (min (frequencies
 
 
