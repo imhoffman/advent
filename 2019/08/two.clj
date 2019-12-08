e22d1d5b0cc41f2c4f648db0620b8a6eb635baff
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-08 10:20:17 -0800

    comments and cleanup

--- 2019/08/two.clj
+++ 2019/08/two.clj
@@ -4,16 +4,29 @@
 (def height 6)
 (def pixels_per_layer (* width height))
 
-;;  rather than `aget` within the other functions
+;;  "array" addressing
+;;  rather than `aget` within actual Java arrays
 (defn get-pixel [layers x y z]
   (get (get (get layers z) y) x))
 
 ;;  initially call with z=0, i.e. looking down from the top
+;;   recursively searches vertically past transparent values
 (defn display-pixel [cube x y z]
   (let [current-layer-value (get-pixel cube x y z)]
     (if (= current-layer-value \2)
       (recur cube x y (inc z))
       current-layer-value)))
+
+;; determine display and write to terminal
+;;  use colours and #/. as per 2016 day 8
+(defn render [cube]
+  (let [display-values (vec
+    (for [y (range height)] (conj []
+      (for [x (range width)]
+        (if (= (display-pixel cube x y 0) \1)
+          "\033[31m\033[1m\033[43m#\033[0m" \.)))))]
+    (doseq [line display-values]
+      (println (map str/join line)))))
 
 ;;
 ;;  main program
@@ -25,7 +38,7 @@
 (println "Read" (count input) "puzzle characters from one line.")
 
 
-;;  parse individual layers from input into a vector
+;;  parse individual layers from input into a vector of vectors
 (def layers
   ((fn [s accum]
     (if (str/blank? s)
@@ -34,9 +47,8 @@
         (recur (subs s pixels_per_layer) (conj accum r)))))
   input []))
 
-
-;;  2D array for display
-(def layers-2d 
+;;  more structure ... data cube: a vector of layers
+(def layers-3d 
   (vec
     (for [layer layers]
       ((fn [s accum]
@@ -46,16 +58,7 @@
        layer []))))
 
 
-;; colours and #/. as per 2016 day 8
-(def display
-  (vec
-    (for [y (range height)] (conj []
-      (for [x (range width)]
-        (if (= (display-pixel layers-2d x y 0) \1)
-          "\033[31m\033[1m\033[43m#\033[0m" \.))))))
-
-;; pretty print
-(doseq [line display]
-    (println (map str/join line)))
+;;  apply ruleset and output to screen
+(render layers-3d)
 
 
