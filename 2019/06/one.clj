5aa7bb33f838411b8dacf73334fc5239be83c32c
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-08 19:37:52 -0800

    assembled (trivial) dictionary of direct orbits

--- 2019/06/one.clj
+++ 2019/06/one.clj
@@ -8,11 +8,19 @@
   (vec (for [listing orbit-map]
          (str/split listing sep))))
 
-(defn bary [pair]
-  (first pair))
+(defn bary [pair] (first pair))
 
-(defn satl [pair]
-  (last pair))
+(defn satl [pair] (last pair))
+
+;;  for determining direct orbits
+;;   call initially with dict as an empty hash map
+(defn direct-orbit-dict [pairs dict]
+  (let [a (bary (first pairs))]
+    (if (empty? a)
+      dict
+      (recur (rest pairs)
+             (assoc dict a (if (contains? dict a) (inc (get dict a)) 1)))
+      )))
 
 
 
@@ -26,8 +34,9 @@
     (reduce conj () (line-seq f))))
 (println "Read" (count input) "lines.")
 
+;;  load up vector of pairs for processing going forward
 (def pairs (parse-orbits input))
 
-(let [pair (get pairs 4)]
-  (println "In" pair "," (satl pair) "orbits" (bary pair) "."))
+
+(println " Number of direct orbits in map:" (apply + (vals (direct-orbit-dict pairs {}))))
 
