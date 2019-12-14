8fce9d3572aaceba5d2b599354001db28ced3cc1
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-13 23:18:29 -0800

    parsed reactions into dictionary

--- 2019/14/one.clj
+++ 2019/14/one.clj
@@ -1,6 +1,34 @@
 (require '[clojure.string :as str])
 
 
+;;  unpack reagents from comma-separated string
+(defn uncomma [string-of-chemicals]
+  (list (str/split (str/replace string-of-chemicals \, \space) #"\s+")))
+
+;;  dictionary of reagents
+;;   key = chemical; value = amount
+(defn make-inputs-dict [ins accum]
+  (if (empty? ins)
+    accum
+    (recur (rest (rest ins)) (assoc accum (second ins) (first ins)))))
+
+
+;;  dictionary of dictionaries of reaction inputs and outputs
+;;   key = {product amt}; value = { {reactant amt} }
+(defn parser [rxns dict]
+  (for [chems 
+        (for [rxn rxns]
+          (let [j    (dec (str/index-of rxn \=))
+                ins  (str/trim (subs rxn 0 j))
+                outs (str/trim (subs rxn (+ j 4)))]
+            (list ins outs)))]
+    (let [ws-ins-list (uncomma (first chems))
+          ws-outs-list (str/split (second chems) #"\s+")]
+      (assoc dict
+             {(second ws-outs-list) (first ws-outs-list)}
+             (make-inputs-dict (first ws-ins-list) {})))))
+
+
 
 ;;
 ;;  main program
@@ -12,13 +40,6 @@
 (println "Read" (count input) "lines.")
 
 
-(def ins-and-outs
-(for [rxn input]
-  (let [j    (dec (str/index-of rxn \=))
-        ins  (subs rxn 0 j)
-        outs (subs rxn (+ j 4))]
-    (list ins outs)))
-   )
+(println (parser input {}))
 
-(doseq [s ins-and-outs] (println s))
 
