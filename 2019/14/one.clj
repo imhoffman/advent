187ee06d932dd7245fa6314d2ed45d6d168e0d64
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-13 21:04:06 -0800

    begin part one

---
+++ 2019/14/one.clj
@@ -0,0 +1,16 @@
+(require '[clojure.string :as str])
+
+
+
+;;
+;;  main program
+;;
+;;   file I/O
+(def input
+  (with-open [f (clojure.java.io/reader "puzzle.txt")]
+    (reduce conj [] (vec (line-seq f)))))
+(println "Read" (count input) "lines.")
+
+
+
+
