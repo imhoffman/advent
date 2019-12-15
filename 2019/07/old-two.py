89ca48f82f4ecfeb463838fab3d0c35b32dbf9e6
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-15 14:39:28 -0800

    start re-writing part two with updated vm

--- 2019/07/old-two.py
+++ 2019/07/old-two.py
@@ -7,152 +7,90 @@ from itertools import permutations
 ##  subprograms
 ##
 
-#  unpack individual integers from compound Intcode
 def parse_opcode ( ABCDE ):
-    A, B, C = 0, 0, 0
     as_char_array = str( ABCDE )
     opcode = int( as_char_array[-1:] )
-    if opcode == 9 and len(as_char_array) > 1 and int( as_char_array[-2:-1] ) == 9:
+    if opcode == 9:
         opcode = 99
     if as_char_array[-3:-2]:
         C = int( as_char_array[-3:-2] )
+    else:
+        C = 0
     if as_char_array[-4:-3]:
         B = int( as_char_array[-4:-3] )
+    else:
+        B = 0
     if as_char_array[-5:-4]:
         A = int( as_char_array[-5:-4] )
-    return opcode, (C, B, A)
+    else:
+        A = 0
+    return opcode, C, B, A
 
 #  for fetching the appropriate value as per the instruction modalities
-#   "Parameters that an instruction writes to will never be in immediate mode."
-def modal_parameters ( opcode, ram, ip, modes, base ):
-    if opcode == 99:
-        return 0, 0, 0
-    # first param; arg1 is the writing location of opcode 3
-    if opcode == 3:
-        if modes[0] == 2:
-            arg1 = ram[ip+1] + base
-        else:
+def modal_parameters ( ram, ip, modes ):
+        if modes[0]:
             arg1 = ram[ip+1]
-    else:
-        if modes[0] == 1:
-            arg1 = ram[ip+1]
-        elif modes[0] == 2:
-            arg1 = ram[ ram[ip+1] + base ]
         else:
             arg1 = ram[ ram[ip+1] ]
-    # second param
-    if modes[1] == 1:
-        arg2 = ram[ip+2]
-    elif modes[1] == 2:
-        arg2 = ram[ ram[ip+2] + base ]
-    else:
-        arg2 = ram[ ram[ip+2] ]
-    # third param; arg3 is the writing location of opcodes 1,2,7,8
-    if modes[2] == 2:
-        arg3 = ram[ip+3] + base
-    else:
+        if modes[1]:
+            arg2 = ram[ip+2]
+        else:
+            arg2 = ram[ ram[ip+2] ]
+        # "Parameters that an instruction writes to will never be in immediate mode."
+        assert modes[2] == 0
         arg3 = ram[ip+3]
-    return arg1, arg2, arg3
+        return arg1, arg2, arg3
 
 
-
-class Amplifier(object):
-    def __init__( self, program, phase_setting ):
-        self.ram = program
-        self.input_value = phase_setting
-        self.phase_not_yet_set = True
-        self.original_program = []
-        self.original_program[:] = program[:]
-        self.ip = 0
-        return
-
-
-    def input_to_program ( self ):
-        if self.phase_not_yet_set:
-            self.phase_not_yet_set = False
+def processor ( ram, ip, input_value ):
+    opcode, mode1, mode2, mode3 = parse_opcode( ram[ip] )
+    output_value = -42   # default 4 or 3 return value ... is this safe?
+    if opcode in ( 1, 2, 5, 6, 7, 8 ):
+        arg1, arg2, arg3 = modal_parameters( ram, ip, (mode1, mode2, mode3) )
+    if   opcode == 1:
+        ram[ arg3 ] = arg1 + arg2
+        return ram, ip+4, output_value
+    elif opcode == 2:
+        ram[ arg3 ] = arg1 * arg2
+        return ram, ip+4, output_value
+    elif opcode == 3:
+        if input_value < 0:
+            output_value = -3   # this means `still running, waiting for input`
+            return ram, ip, output_value    # hold on input ip
         else:
-            self.input_value = new_input
-        return
-
-
-    def output_from_program ( self, value ):
-        if value == 0:
-            self.section_map[ self.requested_position[0] ][ self.requested_position[1] ] = 1
-        elif value == 1:
-            self.move_robot( self.user_input, True )
-        elif value == 2:
-            self.move_robot( self.user_input, True )
-            self.section_map[ self.position[0] ][ self.position[1] ] = 2
-            self.oxygen_location = ( self.position[0], self.position[1] )
-            #print( "Found oxygen system at: ", self.position[0], self.position[1] )
-            #print( " number of unique locations visited: %d\n" % ( len( self.visited) ) )
-            #self.final_render()
+            ram[ ram[ip+1] ] = input_value
+            output_value = -4   # a neg value other than -3 ??
+            return ram, ip+2, output_value
+    elif opcode == 4:
+        output_value = ( ram[ ram[ip+1] ] )
+        return ram, ip+2, output_value
+    elif opcode == 5:
+        if arg1:
+            return ram, arg2, output_value
         else:
-            print( " problem processing output\n" )
-        #self.render()
-        if self.number_of_commands > 10000000:
-            self.final_render()
-        return
-            
-
-    def processor ( self, ram, ip, base_addr ):
-        ram = self.ram
-        ip = self.ip
-        base_addr = self.base_addr
-        opcode, modes = parse_opcode( ram[ip] )
-        arg1, arg2, arg3 = modal_parameters( opcode, ram, ip, modes, base_addr )
-        if   opcode == 1:
-            ram[ arg3 ] = arg1 + arg2
-            return ram, ip+4, base_addr
-        elif opcode == 2:
-            ram[ arg3 ] = arg1 * arg2
-            return ram, ip+4, base_addr
-        elif opcode == 3:
-            input_command = self.input_to_program()
-            ram[ arg1 ] = input_command
-            return ram, ip+2, base_addr
-        elif opcode == 4:
-            self.output_from_program( arg1 )
-            return ram, ip+2, base_addr
-        elif opcode == 5:
-            if arg1:
-                return ram, arg2, base_addr
-            else:
-                return ram, ip+3, base_addr
-        elif opcode == 6:
-            if not arg1:
-                return ram, arg2, base_addr
-            else:
-                return ram, ip+3, base_addr
-        elif opcode == 7:
-            if arg1 < arg2:
-                ram[ arg3 ] = 1
-            else:
-                ram[ arg3 ] = 0
-            return ram, ip+4, base_addr
-        elif opcode == 8:
-            if arg1 == arg2:
-                ram[ arg3 ] = 1
-            else:
-                ram[ arg3 ] = 0
-            return ram, ip+4, base_addr
-        elif opcode == 9:
-            return ram, ip+2, base_addr + arg1
-        elif opcode == 99:
-            return ram, -1, base_addr
+            return ram, ip+3, output_value
+    elif opcode == 6:
+        if not arg1:
+            return ram, arg2, output_value
         else:
-            print("unknown opcode")
-        return
-
-
-    def execute ( self ):
-        while self.ip != -1:
-            self.ram, self.ip, self.base_addr = \
-                    self.processor( self.ram, self.ip, self.base_addr )
-        return
-##  end of Amplifier class
-
-
+            return ram, ip+3, output_value
+    elif opcode == 7:
+        if arg1 < arg2:
+            ram[ arg3 ] = 1
+        else:
+            ram[ arg3 ] = 0
+        return ram, ip+4, output_value
+    elif opcode == 8:
+        if arg1 == arg2:
+            ram[ arg3 ] = 1
+        else:
+            ram[ arg3 ] = 0
+        return ram, ip+4, output_value
+    elif opcode == 99:
+        output_value = -1
+        return ram, -1, output_value
+    else:
+        print("unknown opcode")
 
 
 
@@ -203,6 +141,7 @@ class amplifier:
 
 
 def thrusters( program, phase_settings ):
+
     # "memory is not shared or reused between copies of the program"
     Amp_A = amplifier( program, phase_settings[0] )
     Amp_B = amplifier( program, phase_settings[1] )
@@ -213,7 +152,7 @@ def thrusters( program, phase_settings ):
 
     for amp in amps:
         amp.obtain_input(0)      # initialize phase setting
-    Amp_A.obtain_input(0)        # start to process with a 0 to A
+    Amp_A.obtain_input(0)
     while True:
         Amp_B.obtain_input( Amp_A.generate_output()[0] )
         Amp_C.obtain_input( Amp_B.generate_output()[0] )
