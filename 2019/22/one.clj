0108aec32b941add600b2ca3d5a1b073f589cb71
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-21 23:10:46 -0800

    decide upon data struct for funcs

--- 2019/22/one.clj
+++ 2019/22/one.clj
@@ -1,15 +1,17 @@
 (require '[clojure.string :as str])
 
-
-(defn technique-parser [list-of-techniques]
-  (doseq [technique-string list-of-techniques]
-    (let [technique (str/split technique-string #"\s+")]
-      (cond
-        (and (= (first technique) "deal") (= (second technique) "into")) (println "stack deal")
-        (and (= (first technique) "deal") (= (second technique) "with")) (println "increment" (last technique) "deal")
-        (= (first technique) "cut") (println "cut by" (last technique))
-        :else (println " problem parsing technique")))))
-
+;;  parse the instruction from the puzzle input into a
+;;  two-elem list of a technique and its accompanying integer
+(defn technique-parser [technique-instruction]
+  (let [technique (str/split technique-instruction #"\s+")]
+    (cond
+      (and (= (first technique) "deal") (= (second technique) "into"))
+        (list "deal" -1)
+      (and (= (first technique) "deal") (= (second technique) "with"))
+        (list "increment" (Integer/parseInt (last technique)))
+      (= (first technique) "cut")
+        (list "cut" (Integer/parseInt (last technique)))
+      :else (println " problem parsing technique"))))
 
 
 
@@ -23,5 +25,7 @@
     (reduce conj [] (line-seq f))))           ;; order matters, so vector
 (println "Read" (count input) "lines.")
 
-(technique-parser input)
+;; dummy test
+(doseq [x input]
+  (println (technique-parser x)))
 
