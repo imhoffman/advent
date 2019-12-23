6a747c15ddccd089a06f162d726a4878c4a7e381
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-23 01:59:07 -0800

    needed product, not sum, in increment steps

--- 2019/22/one.clj
+++ 2019/22/one.clj
@@ -19,11 +19,10 @@
 ;;  I'm going to cheat the Lisp gods and make a mutable Java array
 (defn increment-deal [N deck]
   (let [deck-array (into-array Integer/TYPE deck)]
-    (aset deck-array 0 (get deck 0))   ;; leave the first card unchanged
-    (dotimes [i (dec (count deck))]
-      (let [j (mod (+ N i 1) N)]
-        (aset deck-array j (get deck (inc i)))))
-      (into [] deck-array)))
+    (doseq [i (range (count deck))]
+      (let [j (mod (* N i) (count deck))]
+        (aset deck-array j (get deck i))))
+      (into [] deck-array)))   ; back to vector for return
 
 
 
@@ -38,7 +37,6 @@
         (let [n (second technique)]
           (if (< n 0)
             (let [m (+ n (count deck))]
-            ;(let [m (+ n (count deck) 1)]
               (reduce conj (subvec deck m) (subvec deck 0 m)))
             (reduce conj (subvec deck n) (subvec deck 0 n))))
       (= (first technique) "increment")
@@ -59,21 +57,19 @@
 (println "Read" (count input) "lines.")
 
 
-(def number-of-cards 10)
-;(def number-of-cards 10007)
+;(def number-of-cards 10)
+(def number-of-cards 10007)
 
 (def factory-deck (vec (range number-of-cards)))
 
-(def card-to-look-for 9)
+(def card-to-look-for 2019)
 
 (println
   " card" card-to-look-for "is at location"
          (.indexOf
   ((fn [list-of-instructions deck]
-     (println " the deck passed to the lambda:" deck)
     (if (empty? list-of-instructions)
-      (do (println " the entire deck:" deck)
-          deck)
+      deck
       (recur
         (rest list-of-instructions)
         (shuffle-exec (first list-of-instructions) deck))))
