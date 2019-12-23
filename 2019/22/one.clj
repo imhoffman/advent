dc8635c9ded19cf0f941a562e11e96e40d07d3e5
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2019-12-22 20:06:39 -0800

    increment ruleset is not correct

--- 2019/22/one.clj
+++ 2019/22/one.clj
@@ -19,9 +19,10 @@
 ;;  I'm going to cheat the Lisp gods and make a mutable Java array
 (defn increment-deal [N deck]
   (let [deck-array (into-array Integer/TYPE deck)]
-    (dotimes [i (count deck)]
-      (let [j (mod (+ N i) N)]
-        (aset deck-array j (get deck j))))
+    (aset deck-array 0 (get deck 0))   ;; leave the first card unchanged
+    (dotimes [i (dec (count deck))]
+      (let [j (mod (+ N i 1) N)]
+        (aset deck-array j (get deck (inc i)))))
       (into [] deck-array)))
 
 
@@ -32,11 +33,12 @@
   (let [technique (technique-parser instruction)]
     (cond
       (= (first technique) "deal")
-        (vec (reverse deck))    ;; `reverse` return a list, despite a vector input
+        (vec (reverse deck))    ;; `reverse` returns a list, despite a vector input
       (= (first technique) "cut")
         (let [n (second technique)]
           (if (< n 0)
             (let [m (+ n (count deck))]
+            ;(let [m (+ n (count deck) 1)]
               (reduce conj (subvec deck m) (subvec deck 0 m)))
             (reduce conj (subvec deck n) (subvec deck 0 n))))
       (= (first technique) "increment")
@@ -57,21 +59,26 @@
 (println "Read" (count input) "lines.")
 
 
-(def number-of-cards 10007)
+(def number-of-cards 10)
+;(def number-of-cards 10007)
 
 (def factory-deck (vec (range number-of-cards)))
 
+(def card-to-look-for 9)
+
 (println
-  " card 2019 is at location"
+  " card" card-to-look-for "is at location"
          (.indexOf
   ((fn [list-of-instructions deck]
+     (println " the deck passed to the lambda:" deck)
     (if (empty? list-of-instructions)
-      deck
+      (do (println " the entire deck:" deck)
+          deck)
       (recur
         (rest list-of-instructions)
         (shuffle-exec (first list-of-instructions) deck))))
     input factory-deck)
-  2019)
+  card-to-look-for)
 )
 
 ; 1435 is too low
