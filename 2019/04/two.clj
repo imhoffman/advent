5fdd831738cff9c0e607c439cd1742cee2989acc
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-01-15 13:34:19 -0800

    decent draft of new repeats, but not working

--- 2019/04/two.clj
+++ 2019/04/two.clj
@@ -5,33 +5,37 @@
 
 
 ;;  since there are no leading zeros, a zero anywhere
-;;  would break the always-increasing rule
-;;   but this predicate is superfluous
+;;  would break the always-increasing rule, but this
+;;  predicate is superfluous, and is probably slower
+;;  then letting `is-increasing?` catch them...
 (defn no-zeros? [number]
   (not (some #(= \0 %) (str number))))    ; easier to negate here than in filter
 
 
 ;;  using `partition` as seen on (apropos
+;;  updated for part two
 (defn has-repeats? [number]
-  (some #(= (first %) (second %)) (partition 2 1 (str number))))
+  (some
+    #(
+      (let [a (first %)
+            b (second %)
+            c (nth % 2)]    ;; zero-indexed
+        (if (= a b c)
+          false
+          (or (= a b) (= b c)))))
+    (partition 3 1 (str number))))
 
 
 ;;  chars come out of partition,
 ;;  but `>=` requires numbers,
 ;;  and `parseInt` requires strings
 (defn is-increasing? [number]
-  (every?
-    #(
-      (let [a Integer/parseInt (str (first %))
-            b Integer/parseInt (str (second %))
-            c Integer/parseInt (str (nth % 3))]
-        (if (= a b c)    ; this won't work on 111122
-          false
-          (<= a b c))))
-    (partition 2 1 (str number))))
+  (every? #(<=
+           (Integer/parseInt (str (first %)))
+           (Integer/parseInt (str (second %))))
+          (partition 2 1 (str number))))
 
 
-;;  part one
 (defn tally-possibilities [ni nf]
   (let [zero-free-list (filter no-zeros? (range ni nf))]
     (count (filter has-repeats? (filter is-increasing? zero-free-list)))))
@@ -45,7 +49,7 @@
 (let [argv *command-line-args*]
   (if (not (= 2 (count argv)))
     (do
-      (println " usage: clj one.clj lower-limit upper-limit")   ;; $ clj one.clj Int Int
+      (println " usage: clj two.clj lower-limit upper-limit")   ;; $ clj two.clj Int Int
       (System/exit 1))
     (let [lower-limit (Integer/parseInt (first argv))
           upper-limit (Integer/parseInt (second argv))]
