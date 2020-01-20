f747c0b818fb727684d09aac8791a49d2f7d42fd
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-01-20 11:55:28 -0800

    now reading program from external file

--- 2019/02/one.clj
+++ 2019/02/one.clj
@@ -1,19 +1,22 @@
-
-;;  example program from puzzle description
-(def program [1 9 10 3
-              2 3 11 0
-              99
-              30 40 50])
+(require '[clojure.string :as str])
 
 
+;;  program counter lookup
 ;;  dictionary of {opcode, ip increment}
 (def opcode-widths {1 4, 2 4, 99 0})
 
 
 ;;  dictionary of {opcode, function}
+;;   perhaps the keys should be chars like \1, \2
+;;   depending on how to int-as-str shakes out
 (def operations
   {1 #(+ (% 1) (% 2)),
    2 #(* (% 1) (% 2))})
+
+
+;;  perhaps a single, large assoc array ?
+;(def opcodes
+;  {1 {:ip-inc 4, :func #(+ (% 1) (% 2))}
 
 
 ;;  return a vector of the opcode and its arguments
@@ -30,7 +33,22 @@
     ((operations (instruction 0)) instruction)))
 
 
+;;
+;;  main program
+;;
+;;   file I/O
+(def intcode-program
+  (let [file-contents (with-open
+                        [f (clojure.java.io/reader "puzzle.txt")]
+                        (str/trim (slurp f)))]
+    (vec
+     (for [c (str/split file-contents #"[,]")]
+       (Integer/parseInt c)))))
+
+(println "Read" (count intcode-program) "Intcode ints from one line.")
+
 ;;  test print
-(println (operate program 4))
+(println intcode-program)
+(println (operate intcode-program 0))
 
 
