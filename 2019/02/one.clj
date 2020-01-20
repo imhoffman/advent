c934338423c7307f72989c8cbc6413737251f951
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-01-19 21:18:18 -0800

    progress on clojure part one

--- 2019/02/one.clj
+++ 2019/02/one.clj
@@ -1,16 +1,36 @@
 
+;;  example program from puzzle description
 (def program [1 9 10 3
               2 3 11 0
               99
               30 40 50])
 
+
+;;  dictionary of {opcode, ip increment}
+(def opcode-widths {1 4, 2 4, 99 0})
+
+
+;;  dictionary of {opcode, function}
+(def operations
+  {1 #(+ (% 1) (% 2)),
+   2 #(* (% 1) (% 2))})
+
+
+;;  return a vector of the opcode and its arguments
 (defn parse-opcode [ram ip]
-  (let [opcode-widths {1 4, 2 4, 99 1}]
-    (into []
-          (for [n (range ip (+ ip (opcode-widths (get ram ip))))]
-            (get ram n)))))
+  (into []
+        (for
+          [n (range ip (+ ip (opcode-widths (ram ip))))]
+          (ram n))))
 
-;(defn operations {1 #(+ %1 %2), 2 #(* %1 %2)})
 
-(println (parse-opcode program 4))
+;;  apply the opcode function to the arguments
+(defn operate [ram ip]
+  (let [instruction (parse-opcode ram ip)]
+    ((operations (instruction 0)) instruction)))
+
+
+;;  test print
+(println (operate program 4))
+
 
