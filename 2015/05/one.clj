79689613ddaa2dee703e65c42897754cad4f982e
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-04-18 11:29:45 -0600

    three _total_ vowels...

--- 2015/05/one.clj
+++ 2015/05/one.clj
@@ -3,16 +3,24 @@
 (require '[clojure.set    :as set])
 (require '[clojure.string :as str])
 
+
 (defn has-three-vowels? [s]
-  (> (count (set/intersection
-              #{\a \e \i \o \u}
-              (set (keys (frequencies s)))))
-     2))
+  (let [freqs      (frequencies s)
+        vowel-list (into () (set/intersection
+                              #{\a \e \i \o \u}
+                              (set (keys freqs))))]
+    (loop [work-stack vowel-list
+           counter    0]
+      (if (empty? work-stack)
+        (> counter 2)
+        (recur
+          (rest work-stack)
+          (+ counter (freqs (first work-stack))))))))
 
 
 
 (defn free-of-bad-pairs? [s]
-  (if (some true? (for [pair '("ab" "cd" "pq" "xy")]
+  (if (some true? (for [pair (list "ab" "cd" "pq" "xy")]
                     (str/includes? s pair)))
     false
     true))    ;; `some` returns the value, not the logical
