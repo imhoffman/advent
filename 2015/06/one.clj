f4f794573d2b237e034fb1e5f3bcbe0160ffc17a
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-04-19 08:20:35 -0600

    begin day 6

---
+++ 2015/06/one.clj
@@ -0,0 +1,31 @@
+
+(require '[clojure.string :as str])
+
+(def initial-screen
+  (let [dim 1000]
+    (loop [i     0
+           array (transient [])]
+      (if (= i dim)
+        (persistent! array)
+        (recur (inc i) (conj! array (into [] (repeat dim 0))))))))
+
+
+;;  input `s` is a single instruction line from the input file
+;;  return is a vector of
+;;   [operation [x_init,y_init] [x_final,y_final]]
+;;   for subsequent destructuring
+;;  splitting the entire string, only to use `first` to get op-str
+;;   ...how to do it better? subs of an index-of of a regex ??
+(defn parser [s]
+  (let [op-str          (first (str/split s #"\d+"))
+        [xi,yi,_,xf,yf] (vec (str/split (subs s (count op-str)) #"[,\s+]"))
+        long            #(Long/parseLong %)]      ;; overload within scope
+    (vector
+      (case (last (str/trim op-str))  ;; the final letter of the instructions is unique
+        \n :turn-on
+        \f :turn-off
+        \e :toggle)
+      [(long xi) (long yi)]
+      [(long xf) (long yf)])))
+
+
