066a0f6e4785e8f323f72973aaefec7a22ef69b3
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-04-25 15:38:28 -0600

    done with part one

--- 2015/08/one.clj
+++ 2015/08/one.clj
@@ -1,28 +1,32 @@
 
 (require '[clojure.string :as str])
 
-(defn differencer [s]
-  (- (count s)
-     ((frequencies s) \newline)
-     (loop [stack s
-            accum 0]
-       (if (empty? stack)
-         accum
-         (case (first stack)
-           \" (recur (rest stack) (+ accum 1))
-           \\ (case (second stack)
-                \" (recur (rest (rest stack)) (+ accum 1))
-                \\ (recur (rest (rest stack)) (+ accum 1))
-                \x (recur (rest (rest stack)) (+ accum 3)))
-           (recur (rest stack) accum))))))
+(defn differencer [list-of-strings]
+  (let [flattened-master-string-of-all-chars (apply str list-of-strings)]
+    (- (+ 
+         (count flattened-master-string-of-all-chars)
+         (* 2 (count list-of-strings)))   ;; the removed "'s
+       (loop [stack flattened-master-string-of-all-chars
+              accum 0]
+         (if (empty? stack)
+           accum
+           (if (= (first stack) \\)
+             (case (second stack)
+               \" (recur (nthrest stack 2) (inc accum))
+               \\ (recur (nthrest stack 2) (inc accum))
+               \x (recur (nthrest stack 4) (inc accum))
+                  ;; can't happen: "The only escape sequences used..."
+                  (recur (rest stack) accum))
+             (recur (rest stack) (inc accum))))))))
 
 
 (def input-list-of-data-entries
   (with-open [f (clojure.java.io/reader "puzzle.txt")]
-    (reduce conj () (line-seq f))))
+    (reduce conj () (map #(subs % 1 (dec(count %))) (line-seq f)))))
 
-(println " read" (count input-list-of-data-entries) "lines")
+;(println " read" (count input-list-of-data-entries) "lines")
 
+(println " part one:" (differencer input-list-of-data-entries))
 
 ;; 4978 is too high
 ;; 4977 is too high
