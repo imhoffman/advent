a0e976a7da1e19fcd103c8d0b7755b2c70828d01
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-05-14 07:08:39 -0600

    golfier two

--- 2015/02/two.clj
+++ 2015/02/two.clj
@@ -29,3 +29,23 @@
    "puzzle.txt"))
 
 
+(prn
+  ((fn [filename]
+     (->> filename
+          slurp
+          (re-seq #"\w+")
+          (map #(clojure.string/split % #"[x]"))
+          (map #(vec (map (fn [s] (Long/parseLong s)) %)))
+          (map #(vector
+                  (+ (* 2 (% 0) (% 1))
+                     (* 2 (% 0) (% 2))
+                     (* 2 (% 1) (% 2))
+                     (min (* (% 0) (% 1)) (* (% 0) (% 2)) (* (% 1) (% 2))))
+                  (+ (* (% 0) (% 1) (% 2))
+                     (* 2 (min (+ (% 0) (% 1))
+                               (+ (% 0) (% 2))
+                               (+ (% 1) (% 2)))))))
+          (reduce (fn [v1 v2] (vector (+ (v1 0) (v2 0)) (+ (v1 1) (v2 1)))))))
+   "puzzle.txt"))
+
+
