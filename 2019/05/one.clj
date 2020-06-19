01805d8673efa80cd35afdb8ddcff29900eed236
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-06-18 21:00:22 -0600

    opcodes 3 and 4 are working

--- 2019/05/one.clj
+++ 2019/05/one.clj
@@ -9,8 +9,13 @@
 (def opcodes
   {1  {:ip-inc 4, :func #(+ (% 1) (% 2))}
    2  {:ip-inc 4, :func #(* (% 1) (% 2))}
-   3  {:ip-inc 2, :func (fn [& xs] (Long/parseLong (read-line)))}
-   4  {:ip-inc 2, :func #((println " the progam outputs" (% 1)))}
+   3  {:ip-inc 2, :func (fn [&x]
+                          (print " input: ")
+                          (flush)
+                          (Long/parseLong (read-line)))}
+   4  {:ip-inc 2, :func #(let [out (% 1)]
+                           (println " the progam outputs" out)
+                           out)}       ;; also return what is printed
    99 {:ip-inc 1, :func #(identity %)}
    })
 
@@ -18,17 +23,22 @@
 ;;  parse the Intcode instruction
 ;;  return a vector of the opcode and its arguments
 (defn parse-opcode [ram ip]
-  (let [opcode (ram ip)]
-      (case opcode
-        1 (vector opcode
-                  (ram (ram (+ 1 ip)))
-                  (ram (ram (+ 2 ip)))
-                  (ram (+ 3 ip)))
-        2 (vector opcode
-                  (ram (ram (+ 1 ip)))
-                  (ram (ram (+ 2 ip)))
-                  (ram (+ 3 ip)))
-        99 [99])))
+  (let [opcode (ram ip)] 
+    ;(println " parsing" opcode)
+    (case opcode
+      1 (vector opcode
+                (ram (ram (+ 1 ip)))
+                (ram (ram (+ 2 ip)))
+                (ram (+ 3 ip)))
+      2 (vector opcode
+                (ram (ram (+ 1 ip)))
+                (ram (ram (+ 2 ip)))
+                (ram (+ 3 ip)))
+      3 (vector opcode
+                (ram (+ 1 ip)))
+      4 (vector opcode
+                (ram (ram (+ 1 ip))))
+      99 [99])))
 
 
 ;;  execute a single operation
@@ -44,14 +54,18 @@
         opcode-dict  (opcodes opcode)
         operation    (opcode-dict :func)
         ip-increment (opcode-dict :ip-inc)]
+    ;(println " current RAM state:" ram)
     (vector
       (case opcode            ;;  RAM return
-        (99, 4) ram           ;;  unaltered RAM for 99, 4
-                (assoc        ;;  else
+        99 ram                ;;   unaltered RAM for 99
+         4 (do                ;;   side-effect for 4 ...
+             (operation instruction)
+             ram)             ;;   ...then return unaltered RAM
+           (assoc             ;;   else
                   ram (last instruction) (operation instruction)))
       (case opcode            ;;  counter return
-        99      {}            ;;  *** empty IP dict signals halt ***
-                (assoc        ;;  else
+        99      {}            ;;   *** empty IP dict signals halt ***
+                (assoc        ;;   else
                   dict-of-counters
                   :ip (+ ip ip-increment))))))
 
@@ -81,19 +95,9 @@
 (println "Read" (count intcode-program) "Intcode ints from one line.")
 
 
-;;  brute loop through the possibilities
-(doseq [noun (range 1 100)]
-  (doseq [verb (range 1 100)]
-    (when (= 19690720
-             ((run-program               ;; is the RAM vector upon return
-                (#(-> %                  ;; the RAM argument to `run-program`
-                      (assoc 1 noun)
-                      (assoc 2 verb))
-                      intcode-program)
-                {:ip 0, :base 0})        ;; the counter dict argument
-              0))                        ;; retreive the zeroth element of RAM upon return
-      (do
-        (println " success with noun--verb combo" (+ (* 100 noun) verb))
-        (System/exit 0)))))              ;; poor man's bail out
 
+;;  load the program and run it beginning with IP of 0
+(run-program
+  intcode-program
+  {:ip 0, :base 0})
 
