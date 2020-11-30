0e551ba5462432d558d2323adecb9b4ac331f4f3
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-11-29 21:22:55 -0700

    draft ruleset

--- 2015/10/one.clj
+++ 2015/10/one.clj
@@ -6,3 +6,36 @@
 
 (println input-as-vec-of-longs)
 
+;;
+;;  the ruleset
+;;   returns a vector of longs
+;;
+(defn look-and-say [vector-of-longs]
+  (loop [stack vector-of-longs
+         accum []]
+    (if (empty? stack)
+      accum
+      (let [look (loop [s stack
+                        c 0]
+                   (println " s:" s ", c:" c)
+                   (if true
+                   ;(if (or (= c (count s)) (not= (s c) (s (inc c))))
+                     (vector (first stack) c)
+                     (recur
+                       (vec (rest s))
+                       (inc c))))
+            say  (let [digit (look 0)
+                       times (look 1)]
+                   (loop [c   0
+                          out []]
+                     (if (= c times)
+                       out
+                       (recur
+                         (inc c)
+                         (conj out digit)))))]
+        (recur
+          (vec (nthrest stack (dec (look 1))))
+          (concat accum say))))))
+
+(println (look-and-say input-as-vec-of-longs))
+
