87aed487b4019faa696c679a59063edc15f0e5b6
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-02 22:59:38 -0700

    stuck on part two

---
+++ 2020/03/two.clj
@@ -0,0 +1,46 @@
+
+(require '[clojure.string :as str])
+
+(def tree-map
+  ((fn [s] (-> s
+               slurp
+               (str/split #"\s")
+               vec))
+   "puzzle.txt"))
+
+
+;;  given
+(def slopes
+  [ [1 1], [3 1], [5 1], [7 1], [1 2] ] )
+
+
+(defn product [input vec-of-slope-vecs]
+  (let [v     (vec (map vec input))
+        ncols (count (input 0))
+        nrows (count input)]
+    (loop [stack vec-of-slope-vecs
+           accum 1]
+      (if (empty? stack)
+        accum
+        (recur
+          (rest stack)
+          (* accum
+            (loop [row   0
+                   col   0
+                   trees 0]
+              (if (> (+ row ((first stack) 1)) nrows)
+                trees
+                (recur
+                  (+ row ((first stack) 1))
+                  (+ col ((first stack) 0))
+                  (if (= ((v row) (mod col ncols)) \#)
+                    (inc trees)
+                    trees))))))))))
+
+(println (product tree-map slopes))
+
+
+
+;; 3413906000
+;; 3435931200
+
