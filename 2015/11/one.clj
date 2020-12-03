1a48dcc5a21721eaa2baa901c3b9a533ee4938d9
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-03 13:58:19 -0700

    more part one

--- 2015/11/one.clj
+++ 2015/11/one.clj
@@ -12,9 +12,9 @@
   (and
     (not (re-seq #"(i|o|l)" s))                           ;;  bad letters
     (<= 2 (count (set (map first (re-seq #"(.)\1" s)))))  ;;  two or more different doubles
-    (loop [i 0]
-      (cond
-        (= i (- (count s) 3))
+    (loop [i 0]                                           ;;  straight
+      (cond                                               ;;   bad letters are gone by now,
+        (= i (- (count s) 3))                             ;;   but stuff like "ghj" must still fail
           false
         (str/includes? alphabet (subs s i (+ i 3)))
           true
@@ -22,7 +22,22 @@
           (recur (inc i))))))
 
 
-;(defn increment-password [s]
+(defn increment-password [s]
+  (let [p (vec "abcdefghjkmnpqrstuvwxyz")  ;;  remove the bad letters now, rather than
+        n (dec (count p))]                 ;;   waste the validator's time
+    (loop [stack (reverse s)
+           accum []]
+      (if (empty? stack)
+        (str/join (reverse accum))
+        (let [next-index (mod (inc (str/index-of (first stack))) n)]
+          (recur
+            (if (= 0 next-index)
+              (loop ;over letters until no carry... then return rest of that stack
+                )
+              (rest stack))  ;; no carry needed
+            (conj accum (p next-index))))))))
+
+
 
 
 
