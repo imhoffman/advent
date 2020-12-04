b37ed71a7fdc20a5a4be41dafa1267cba0b25020
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-04 07:48:25 -0700

    still stuck

--- 2020/04/two.clj
+++ 2020/04/two.clj
@@ -2,8 +2,9 @@
 (require '[clojure.string :as str])
 
 (defn parser [r]
-  (let [groups (re-seq #"((\w+):([#|\w]+))\s*" r)     ;;  need to match beyond '#' !!!
+  (let [groups (re-seq #"((\w+):([#|\w]+))\s*" r)
         dict   (into {} (for [e groups] (vector (e 2) (e 3))))]
+        ;dict   (into {} (for [e groups] (vector (e 2) (str/trim (e 3)))))]
     dict))
 
 (def batch
@@ -19,6 +20,7 @@
 
 (defn valid? [record-dict]
   (if (reduce #(and %1 %2) (for [f mandatory-fields] (contains? record-dict f)))
+    (do (println " considering:" record-dict)
     (let [byr (Long/parseLong (record-dict "byr"))
           iyr (Long/parseLong (record-dict "iyr"))
           eyr (Long/parseLong (record-dict "eyr"))
@@ -26,19 +28,21 @@
           hcl (record-dict "hcl")
           ecl (record-dict "ecl")
           pid (record-dict "pid")]
+      (do (println byr iyr eyr hgt hcl ecl pid)
       (and
         (and (>= byr 1920) (<= byr 2002))
         (and (>= iyr 2010) (<= iyr 2020))
         (and (>= eyr 2020) (<= eyr 2030))
-        (let [[_ ns u] (re-matches #"(\d+)(\w+)" hgt)
-              n (Long/parseLong ns)]
-          (if (= u "cm")
-            (and (>= n 150) (<= n 192))
-            (and (>= n 59)  (<= n 76))))
+        (when (re-matches #"\d{2,3}[cmin]{2}" hgt)
+          (let [[_ ns u] (re-matches #"(\d+)(\w+)" hgt)
+                n (Long/parseLong ns)]
+            (if (= u "cm")
+              (and (>= n 150) (<= n 192))
+              (and (>= n 59)  (<= n 76)))))
         (re-matches #"#[0-9|a-f]{6}" hcl)
         (some #(= ecl %) ["amb" "blu" "brn" "gry" "grn" "hzl" "oth"])
         (re-matches #"[0-9]{9}" pid)
-        true))
+        true))))
     false))
 
 
@@ -47,4 +51,6 @@
     (count (filter true? (for [d batch-dicts] (valid? d))))))
 
 ;(doseq [b batch] (println b "\n" (parser b) "\n"))
+
+;; 118 too low
 
