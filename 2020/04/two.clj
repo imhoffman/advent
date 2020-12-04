7f9d7037f47259d018ea67ce86cd75ae733063aa
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-04 10:53:28 -0700

    done with part two: one pad parse somewhere

--- 2020/04/two.clj
+++ 2020/04/two.clj
@@ -3,8 +3,8 @@
 
 (defn parser [r]
   (let [groups (re-seq #"((\w+):([#|\w]+))\s*" r)
-        ;dict   (into {} (for [e groups] (vector (e 2) (e 3))))]
-        dict   (into {} (for [e groups] (vector (e 2) (str/trim (e 3)))))]
+        dict   (into {} (for [e groups] (vector (e 2) (e 3))))]
+        ;dict   (into {} (for [e groups] (vector (e 2) (str/trim (e 3)))))]
     dict))
 
 (def batch
@@ -19,8 +19,8 @@
 
 
 (defn valid? [record-dict]
+  (println "\n considering:" record-dict)
   (if (reduce #(and %1 %2) (for [f mandatory-fields] (contains? record-dict f)))
-    (do (println " considering:" record-dict)
     (let [byr (Long/parseLong (record-dict "byr"))
           iyr (Long/parseLong (record-dict "iyr"))
           eyr (Long/parseLong (record-dict "eyr"))
@@ -33,27 +33,43 @@
         (and (>= byr 1920) (<= byr 2002))
         (and (>= iyr 2010) (<= iyr 2020))
         (and (>= eyr 2020) (<= eyr 2030))
-        (let [matches (re-matches #"(\d{2,3})([cmin]{2}).*" hgt)]
+        (let [matches (re-matches #"(\d{2,3})([cmin]{2})" hgt)]
           (when matches
             (let [[_ ns u] matches
                   n (Long/parseLong ns)]
               (println " hgt matches:" ns u)
               (cond
-                (= u "cm") (and (>= n 150) (<= n 192))
+                (= u "cm") (and (>= n 150) (<= n 193))
                 (= u "in") (and (>= n 59)  (<= n 76))
                 :else false))))
         (re-matches #"#[0-9|a-f]{6}" hcl)
         (some #(= ecl %) ["amb" "blu" "brn" "gry" "grn" "hzl" "oth"])
         (re-matches #"[0-9]{9}" pid)
-        true))))
+        true)))  ;;  so that I can filter on true
     false))
 
 
 (let [batch-dicts (map parser batch)]
-  (println
-    (count (filter true? (for [d batch-dicts] (valid? d))))))
+;  (println
+;    (count (filter true? (for [d batch-dicts] (valid? d))))))
+  (println "\n total count:"
+  (loop [stack batch-dicts
+         c     0]
+    (if (empty? stack)
+      c
+      (if (valid? (first stack))
+        (do (println " PASS:" (first stack))
+            (recur (rest stack) (inc c)))
+        (do (println " FAIL:" (first stack))
+            (recur (rest stack) c)))))
+  )
+  )
+
 
 ;(doseq [b batch] (println b "\n" (parser b) "\n"))
 
 ;; 118 too low
+;; 119 after `trim`ming and adding trailing `.*` to regexs
+;; 122 after fixing cm height typo
+;; 121 correct: after removing `trim`ming ...
 
