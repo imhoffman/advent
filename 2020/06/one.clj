bf9bd0637eba55e995db3799beb762f8b3b89fac
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-05 22:35:46 -0700

    done with part one

---
+++ 2020/06/one.clj
@@ -0,0 +1,25 @@
+
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+
+(def input-sets
+  (->> "puzzle.txt"
+       slurp
+       ;#(str % "\n\n")            ;;  edited the input file ...
+       (re-seq #"([a-z]+\n)+\n")
+       (map first)
+       (map #(str/split % #"\n"))
+       (map #(apply concat %))
+       (map set)))
+
+
+;(prn (nth input-sets 2))
+
+(prn (apply + (for [s input-sets] (count s))))
+
+;(doseq [s input-sets] (prn (count s)))
+
+
+;; 1603
+;; 6694
+
