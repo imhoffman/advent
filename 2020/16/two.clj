f93f218eda87c9d4b7bf27f11f0aa6307e08992c
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-16 19:37:50 -0700

    my logic must be wrong on the rule search

--- 2020/16/two.clj
+++ 2020/16/two.clj
@@ -55,29 +55,23 @@
 ;(prn departure-rules)
 
 
+(def non-departure-rules
+  (->> "puzzle.txt"
+       slurp
+       (#(str/split % #"\n"))
+       (map #(re-matches #"^(?!departure).*: (\d+)-(\d+) or (\d+)-(\d+)" %))
+       (filter #(not (nil? %)))
+       (map rest)
+       (map (fn [s] (map #(Long/parseLong %) s)))
+       (map vec)))
 
-(defn valid? [rules ticket]
-  (loop [stack ticket]
-    (cond
-      (empty? stack)
-        true
-      (let [n (first stack)]
-        (not-every? false?
-          (for [rule rules]
-            (or
-              (and (>= n (rule 0))
-                   (<= n (rule 1)))
-              (and (>= n (rule 2))
-                   (<= n (rule 3)))))))
-        (recur (rest stack))
-      :else
-        false)))
+;(prn non-departure-rules)
 
 
 ;;
-;;  return list of fields that might be departure
+;;  return list of fields that satisfy at least one of `rules`
 ;;
-(defn departure-fields [rules ticket]
+(defn possible-fields [rules ticket]
   (loop [stack  ticket
          field  0
          fields []]
@@ -101,16 +95,44 @@
 
 
 
+;;
+;;  validation predicate
+;;
+(defn valid? [rules ticket]
+  (loop [stack ticket]
+    (cond
+      (empty? stack)
+        true
+      (let [n (first stack)]
+        (not-every? false?
+          (for [rule rules]
+            (or
+              (and (>= n (rule 0))
+                   (<= n (rule 1)))
+              (and (>= n (rule 2))
+                   (<= n (rule 3)))))))
+        (recur (rest stack))
+      :else
+        false)))
+
+
+;;
 ;;  assemble set of fields that are departure and apply * to my-ticket
-(loop [stack (filter (partial valid? input-rules) nearby-tickets)
-       out (set (range (count (first nearby-tickets))))]
-  (if (empty? stack)
-    (let [v (vec my-ticket)]
-      (prn out)
-      (prn (apply * (for [i out] (biginteger (v i))))))
-    (recur
-      (rest stack)
-      (set/intersection out (set (departure-fields departure-rules (first stack)))))))
+;;
+(let [set-of-all-fields (set (range (count (first nearby-tickets))))]
+  (loop [stack (filter (partial valid? input-rules) nearby-tickets)
+         out   set-of-all-fields]
+    (if (empty? stack)
+      (let [v (vec my-ticket)]
+        (prn out)
+        (prn (apply *
+                    (for [i (set/difference set-of-all-fields out)]
+                          (biginteger (v i))))))
+      (recur
+        (rest stack)
+        (set/intersection
+          out
+          (set (possible-fields non-departure-rules (first stack))))))))
 
 
 
