ccbc3d43963e24ec9c6b0339bea3bdac6f68a626
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-21 23:04:39 -0700

    works for test case...need to catch repeat situation

---
+++ 2020/22/two.clj
@@ -0,0 +1,58 @@
+
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+
+(def two-decks
+  (->> "puzzle.txt"
+       slurp
+       (#(str/split % #"\n\n") ,,)
+       (map #(str/split % #"\n") ,,)
+       (map rest ,,)))
+
+(def p1 (mapv #(Long/parseLong %) (first two-decks)))
+(def p2 (mapv #(Long/parseLong %) (second two-decks)))
+
+
+(defn play [deck1 deck2]
+  (loop [d1 deck1
+         d2 deck2]
+    (cond
+      (or (empty? d1) (empty? d2))
+        (vector d1 d2)
+      (and (> (count d1) (first d1)) (> (count d2) (first d2)))
+        (let [[rd1 rd2] (play (vec (rest d1)) (vec (rest d2)))]
+          (if (empty? rd1)
+            (recur
+              (vec (rest d1))
+              (conj (vec (rest d2)) (first d2) (first d1)))
+            (recur
+              (conj (vec (rest d1)) (first d1) (first d2))
+              (vec (rest d2)))))
+      (> (first d1) (first d2))
+        (recur
+          (conj (vec (rest d1)) (first d1) (first d2))
+          (vec (rest d2)))
+      (> (first d2) (first d1))
+        (recur
+          (vec (rest d1))
+          (conj (vec (rest d2)) (first d2) (first d1)))
+      :else
+        (println "war!"))))
+
+
+(let [winner (filter #(not (empty? %)) (play p1 p2))]
+  (println (first winner))
+  (println (loop [d (reverse (first winner))
+                  i 1
+                  accum 0]
+         (if (empty? d)
+           accum
+           (recur
+             (rest d)
+             (inc i)
+             (+ accum (* i (first d))))))))
+
+
+;; 17923
+
+
