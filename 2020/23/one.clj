0ba0e9f13b0abee980a6f6132db5d5eae0643860
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2020-12-23 21:10:05 -0700

    done with part one: off-by-one

--- 2020/23/one.clj
+++ 2020/23/one.clj
@@ -12,29 +12,41 @@
 (prn input)
 
 
-(let [nmoves 10]
+(let [nmoves 100]
   (loop [circle  input
          current (first input)
          moves   0]
-    (println " circle:" circle)
     (let [ci        (.indexOf circle current)
           wrap      (vec (concat circle circle))
-          pickup    (subvec wrap (inc ci) (+ ci 4))
+          pickup    (subvec wrap (+ 1 ci) (+ ci 4))
           leave     (vec (concat
-                      (vector (first circle))
-                      (subvec wrap (+ ci 4) (+ (+ ci 4) (- (count circle) 4)))))
-          di        (loop [stack leave
+                      (subvec circle ci (+ 1 ci))
+                      (subvec wrap
+                              (+ ci 4)
+                              (+ ci 4 (- (count circle) 4)))))
+          [dest di] (loop [stack leave
                            d     (if (= 0 (dec current)) (count input) (dec current))]
                       (let [found (.indexOf stack d)]
                         (if (= -1 found)
                           (recur (rest stack) (if (= 0 (dec d)) (count input) (dec d)))
-                          found)))
+                          (vector d (.indexOf leave d)))))
           new-circ  (vec (concat
                       (subvec leave 0 (+ 1 di))
+                      ;(subvec (vec (concat leave leave)) 0 (+ 1 di))
                       pickup
-                      (subvec leave (inc di))))
+                      (subvec leave (+ 1 di))))
+                      ;(subvec (vec (concat leave leave))
+                      ;        (+ 1 di)
+                      ;        (+ 1 di (- (count leave) (+ 1 di))))))
           new-curr  (new-circ (mod (inc (.indexOf new-circ current)) (count new-circ)))]
-      (if (= moves nmoves)
+      ;(println "\n  current:" current)
+      ;(println "   circle:" circle)
+      ;(println "   pickup:" pickup)
+      ;(println "    leave:" leave)
+      ;(println "     dest:" dest)
+      ;(println " put down:" new-circ)
+      ;(println " next cur:" new-curr)
+      (if (= moves (dec nmoves))
         (let [final-wrap (vec (concat new-circ new-circ))
               i1  (.indexOf final-wrap 1)
               out (apply str (subvec final-wrap (inc i1) (+ i1 (count new-circ))))]
