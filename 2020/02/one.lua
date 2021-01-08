d19e4af074e3df54c069b605651c621f647b7585
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2021-01-07 20:08:56 -0700

    done with lua part one

--- 2020/02/one.lua
+++ 2020/02/one.lua
@@ -8,8 +8,40 @@ for line in io.lines() do
 			 captures[3], captures[4] } )
 end
 
-
+--[[
 for i = 1, #input do
   print( input[i][2] )
 end
+--]]
+
+--[[
+function print_table (t)
+  for k,v in pairs(t) do
+    print(k,v)
+  end
+end
+--]]
+
+function frequencies ( s, accum )
+  local c = s:match("(%a)") 
+  if c then
+    local g = {s:gsub(c,0)}
+    accum[c] = g[2]
+    frequencies( g[1], accum )
+  end
+  return accum
+end
+
+
+nvalid = 0
+for i = 1, #input do
+  local t = input[i]
+  local nmin, nmax, c, s = t[1], t[2], t[3], t[4]
+  local ccount = frequencies( s, {} )[c]
+  if ( ccount and ccount >= nmin and ccount <= nmax ) then
+    nvalid = nvalid + 1
+  end
+end
+
+print(nvalid)
 
