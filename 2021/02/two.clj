f07ac236cafa915474652f04f8ae3a4ca8da3394
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2021-12-01 22:16:24 -0700

    done with part two

---
+++ 2021/02/two.clj
@@ -0,0 +1,33 @@
+
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+
+
+(def input
+  (->> "puzzle.txt"
+       slurp
+       (#(str/split % #"\n") ,,)
+       (map #(re-matches #"(\w+) (\d+)" %) ,,)))
+
+
+(loop [stack input
+       x 0
+       d 0
+       aim 0]
+  (if (empty? stack)
+    (println (* x d))
+    (let [dir ((first stack) 1)
+          a (Integer/parseInt ((first stack) 2))]
+      (recur
+        (rest stack)
+        (if (= "forward" dir)
+          (+ x a)
+          x)
+        (if (= "forward" dir)
+          (+ d (* aim a))
+          d)
+        (case dir
+          "up" (- aim a)
+          "down" (+ aim a)
+          aim)))))
+
