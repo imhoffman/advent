77042ef126fe564a64794834b67488e40501bca9
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2021-12-06 11:26:39 -0700

    done with part two

--- 2021/06/two.clj
+++ 2021/06/two.clj
@@ -12,12 +12,27 @@
 
 
 
-(let [f (frequencies input)
-      ts (keys f)]
-  (println
-    (apply +
-           (for [t ts] (* (f t) (quot 256 t)))
-           ;; some other term with the additional 8s
-           )))
+(loop [day 1
+       fish (loop [i 0
+                   o (frequencies input)]
+              (if (= i 9)
+                o
+                (recur
+                  (inc i)
+                  (if (contains? o i) o (assoc o i 0)))))]
+  (if (= day 257)
+    (println (apply + (vals fish)))
+    (recur
+      (inc day)
+      (loop [t 8
+             d fish]
+        (if (= t 0)
+          (assoc d 8 (fish 0), 6 (+ (fish 7) (fish 0)))
+          (recur
+            (dec t)
+            (assoc d (dec t) (fish t))))))))
+
+
+
 
 
