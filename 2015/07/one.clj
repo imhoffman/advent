a7a698918b53c9681eceb999c2fb15b3bf754f6f
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2022-11-01 20:16:08 -0600

    fix blank regex parses

--- 2015/07/one.clj
+++ 2015/07/one.clj
@@ -20,23 +20,23 @@
 ;;   for NOT, enter the argument as an input
 ;;
 (defn parser [rev]
-  (let [v  (vec (rest rev))
+  (let [v  (filterv (complement empty?) (rest rev))
         pd (cond
              ;;  NOT is the only leading op
              (= (v 0) "NOT")
              {:inputs (vector (v 1)), :op (v 0), :output (v 2)}
 
-             ;;  numerical literal assigmments
-             ;;   there are also literal inputs to binary OPs...
-             (and (= 2 (count v)) (re-matches #"\d+.*" (v 0)))
+             ;;  assignments, literal or symbolic
+             (= 2 (count v))
              {:inputs (vector (v 0)), :op nil, :output (v 1)}
 
-             ;;  normal val OP val structure
+             ;;  normal `val OP val` structure
              :else
              {:inputs (vector (v 0) (v 2)), :op (v 1), :output (v 3)})]
-    ;; find the numbers ...
-    ;;(#(Integer/parseInt (second %)) ,,))
-    pd))
+    ;;  search for and parse any numeral strings
+    ;;   which can only appear in the inputs
+    (assoc pd :inputs
+           (mapv #(if (re-matches #"\d+" %) (Integer/parseInt %) %) (pd :inputs)))))
 
 
 (def input
@@ -47,8 +47,57 @@
        (map parser ,,)))
 
 
+(defn op [inputs operation]
+  (case operation
+    "NOT"
+    (bit-not (inputs 0))
+
+    "AND"
+    (bit-and (inputs 0) (inputs 1))
+
+    "OR"
+    (bit-or (inputs 0) (inputs 1))
+
+    "RSHIFT"
+    (bit-shift-right (inputs 0) (inputs 1))
+
+    "LSHIFT"
+    (bit-shift-left (inputs 0) (inputs 1))
+
+    nil
+    (inputs 0)
+
+    :fail))
+
+
+(def values
+  (reduce
+    (fn [a b]
+      (let [ins (b :inputs)]
+        (assoc
+          a
+          (b :output)
+          (cond
+            (every? number? ins)
+            (op ins (b :op))
+
+            :else
+            nil))))
+    {} input))
+
+
 ;;
 ;;  main
 ;;
 
 (println input)
+
+(println values)
+
+
+
+
+
+
+
+
