a6f65753443d7b2ed9ca4256c0884e09db2cfbac
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2023-08-21 15:12:16 -0600

    done with part one

--- 2022/10/one.clj
+++ 2022/10/one.clj
@@ -9,7 +9,37 @@
                 (map #(vec (rest %)) ,,)
                 (map #(if (% 1) (vector (% 0) (Integer/parseInt (% 1))) %) ,,)))
 
-(prn input)
+;(prn input)
+
+;;  memoize this
+(defn execute [program stop-time]
+  (let [wall-clock        1
+        instruction-timer 0
+        register          1]
+    (loop [p  program
+           wc wall-clock
+           ic instruction-timer
+           x  register]
+      ;(println (first p) ic x)
+      (if (= wc stop-time)
+        x
+        (let [instr      ((first p) 0)
+              [reg,tick] (if (= instr "noop")
+                           (vector x 0)
+                           (if (= ic 1)
+                             (vector (+ x ((first p) 1)) 0)
+                             (vector x (inc ic))))]
+            (recur (if (= tick 0) (rest p) p) (inc wc) tick reg))))))
+
+
+;;
+;;  main
+;;
+(println
+  (apply +
+         (for [n (range 20 221 40)]
+           (* n (execute input n)))))
+
 
 
 
