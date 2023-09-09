519c1fc4ec602987ab88b3a5dc6ec5ac0f612e99
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2023-09-09 14:19:36 -0700

    coffee shop work

--- 2022/21/one.clj
+++ 2022/21/one.clj
@@ -14,10 +14,31 @@
 
 ;(prn input)
 
-(println
-  ((resolve (read-string "+")) 3 5 7))
 
 
+(defn find-value [d k]
+  (let [v1 (first  (d k))
+        v2 (second (d k))
+        v3 (last   (d k))]
+    (println d)
+    (cond
+      (and (number? v1) (nil? v2))
+      v1
+
+      (let [v (or (number? (d v1)) (number? (d v3)))]
+      (and ((complement number?) v1) (nil? v2))
+      (recur (assoc d v1 (list (d v1))) k)
+
+      (and (number? (d v1)) (number? (d v3)))
+      (find-value (assoc d k (list ((resolve (read-string v2)) (d v1) (d v3)))) k)
+
+      :default
+      (recur
+        (find-value d (or ((complement number?) v1) ((complement number?) v3)))
+        k))))
+
+
+(println (find-value input "ptdq"))
 
 
 
