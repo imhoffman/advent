50b6319f9901932d2aad3c3c4bc980cec560e8fa
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2023-12-05 22:22:37 -0700

    done with part one

---
+++ 2023/06/one.clj
@@ -0,0 +1,30 @@
+
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+(require '[clojure.core.reducers :as r])
+
+(def input (->> "puzzle.txt"
+                slurp
+                (#(str/split % #"\n") ,,)
+                (map #(re-seq #"\d+" %) ,,)
+                (map (fn [s] (map #(Integer/parseInt %) s)) ,,)
+                (apply mapv vector ,,)))
+
+(prn input)
+
+(defn successful-cases [pair]
+  (let [d (second pair)]
+    (loop [times (range 1 (first pair))
+           c 0]
+      (if (empty? times)
+        c
+        (let [t (first times)]
+          (recur
+            (rest times)
+            (if (< d (* (- (first pair) t) t)) (inc c) c)))))))
+
+(println
+         (apply * (for [pair input] (successful-cases pair))))
+
+
+
