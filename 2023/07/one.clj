c3bd5b392883a2df727ccf314e58f6bf68bcd270
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2023-12-16 23:00:34 -0700

    chipping away at this one

--- 2023/07/one.clj
+++ 2023/07/one.clj
@@ -11,9 +11,10 @@
                 (map #(vector (first %) (Integer/parseInt (second %))) ,,)
                 (into {} ,,)))
 
-(prn input)
+;(prn input)
 
-(def ranks {\A 0, \K 1, \Q 2, \J 3, \T 4, \9 5, \8 6, \7 7, \6 8, \5 9, \4 10, \3 11, \2 12, \1 13})
+(def ranks {\A 14, \K 13, \Q 12, \J 11, \T 10, \9 9, \8 8, \7 7, \6 6, \5 5, \4 4, \3 3, \2 2, \1 1})
+
 
 (defn ranker [pair1 pair2]
   (let [h1 (first pair1)
@@ -24,22 +25,58 @@
         m2 (apply max (vals f2))
         v1 (vec h1)
         v2 (vec h2)]
-    (prn v1 v2)
+    ;;  tie-break routine should really be letfn'ed ...
+    ;    (loop [i 0]
+    ;              (cond
+    ;                (> (ranks (v1 i)) (ranks (v2 i))) +1
+    ;                (< (ranks (v1 i)) (ranks (v2 i))) -1
+    ;                :default (recur (inc i)))))))
     (cond
-      (> m1 m2) 1
-      (< m1 m2) 0
-      ;(= 2 (count (frequencies (vals 
-      :default  (loop [i 0]
-                  (cond
-                    (and (= 1 m1) (> (apply min (map ranks v1)) (apply min (map ranks v2)))) 1
-                    (and (= 1 m1) (< (apply min (map ranks v1)) (apply min (map ranks v2)))) 0
-                    (> (ranks (v1 i)) (ranks (v2 i))) 1
-                    (< (ranks (v1 i)) (ranks (v2 i))) 0
-                    :default (recur (inc i)))))))
+      ;;  full-house possiblities
+      (and (= '(2 3) (sort (vals f1))) (not= '(2 3) (sort (vals f2)))) +1
+
+      (and (= '(2 3) (sort (vals f2))) (not= '(2 3) (sort (vals f1)))) -1
+
+      (and (= '(2 3) (sort (vals f2))) (= '(2 3) (sort (vals f1))))
+      (if (> (apply min (map ranks v1)) (apply min (map ranks v2))) -1 +1)
+
+      ;;  five-of-a-kind beats four- beats three- beats two-
+      (> m1 m2) +1
+
+      (< m1 m2) -1
+
+      ;;  two pair in each
+      (and (= '(1 2 2) (sort (vals f2))) (= '(1 2 2) (sort (vals f1))))
+      ;; tie-break, but what follows is wrong, use the letfn
+      (if (> (apply min (map ranks v1)) (apply min (map ranks v2))) -1 +1)
+
+      ;;  one pair in each
+      (and (= '(1 1 1 2) (sort (vals f2))) (= '(1 1 1 2) (sort (vals f1))))
+      ;; tie-break, but what follows is wrong, use the letfn
+      (if (> (apply min (map ranks v1)) (apply min (map ranks v2))) -1 +1)
+
+      ;;  high card in each
+      (and (= 1 m1) (= 1 m2))  ;;  or simply (= 1 m1)
+      ;; tie-break, but what follows is wrong, use the letfn
+      (if (> (apply min (map ranks v1)) (apply min (map ranks v2))) -1 +1)
+
+      :default 0)))
+
 
 (println
-  (sort ranker input))
+  (loop [v (sort ranker input)
+         i 1
+         c 0]
+    (if (empty? v)
+      c
+      (recur
+        (rest v)
+        (inc i)
+        (+ c (* i (second (first v))))))))
 
 
+;;  254609753 too high
+;;  247280583 too high
+;;  246390747 too low
 
 
