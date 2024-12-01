a913810cb7a815789b1e193bad55c3d470f12ab9
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2024-11-30 21:56:01 -0700

    dummy file

---
+++ 2024/01/one.clj
@@ -0,0 +1,16 @@
+
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+
+(def input (->> "puzzle.txt"
+                slurp
+                (#(str/split % #"\n") ,,)
+                (map #(Integer/parseInt %) ,,)))
+
+;(println input)
+
+(println (apply + input))
+
+
+
+
