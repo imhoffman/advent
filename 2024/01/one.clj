630f0b9d75e08f9dd54a1f67fb8149734c732e57
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2024-11-30 21:54:04 -0700

    2024 dirs

---
+++ 2024/01/one.clj
@@ -0,0 +1,37 @@
+
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+
+(def d {"zero" "0",
+        "one" "1",
+        "two" "2",
+        "three" "3",
+        "four" "4",
+        "five" "5",
+        "six" "6",
+        "seven" "7",
+        "eight" "8",
+        "nine" "9"})
+
+(def input (->> "puzzle.txt"
+                slurp
+                (#(str/split % #"\n") ,,)
+                (map #(loop [s (vec %), o (vector)]
+                        (if (empty? s)
+                          o
+                          (recur
+                            (vec (rest s))
+                            (let [m (re-matches #"(^\d|zero|one|two|three|four|five|six|seven|eight|nine).*" (apply str s))]
+                              (if m (conj o (second m)) o))))) ,,)
+                ;(map #(re-seq #"(?:\d|zero|one|two|three|four|five|six|seven|eight|nine)?" %) ,,)
+                (map #(reduce (fn [a b] (conj a (if (d b) (d b) b))) [] %) ,,)
+                (map #(str (first %) (last %)) ,,)
+                (map #(Integer/parseInt %) ,,)))
+
+;(println input)
+
+(println (apply + input))
+
+
+
+
