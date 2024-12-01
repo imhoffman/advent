c34b0c0c040f32e8ca646b6e27edbbdb8ed42459
Ian M. Hoffman <hoffman.ian.m@gmail.com>
2024-11-30 22:22:24 -0700

    done with part one

--- 2024/01/one.clj
+++ 2024/01/one.clj
@@ -4,13 +4,21 @@
 
 (def input (->> "puzzle.txt"
                 slurp
-                (#(str/split % #"\n") ,,)
-                (map #(Integer/parseInt %) ,,)))
+                ;(#(str/split % #"\n") ,,)
+                (re-seq #"(\d+)\s*?(\d+)\n" ,,)
+                (map rest ,,)
+                (reduce (fn [a b]
+                            (vector
+                            (conj (nth a 0) (Integer/parseInt (nth b 0)))
+                            (conj (nth a 1) (Integer/parseInt (nth b 1)))))
+                        [[][]] ,,)
+                (map sort ,,)))
 
 ;(println input)
 
-(println (apply + input))
-
-
+(println
+  (reduce
+    (fn [a b]
+      (+ a (abs (- (nth b 0) (nth b 1))))) 0 (map vector (nth input 0) (nth input 1))))
 
 
